fdafc9757eea498b1f9cdb1b0374d35e2e5bbfe7
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2024-09-05 20:55:27 +0530

    Time: 10 ms (65.87%), Space: 11.2 MB (29.19%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -3,32 +3,25 @@ public:
     string frequencySort(string s) {
         int n = s.size();
 
-        // Count frequencies
         unordered_map<char, int> freq;
 
-        for (auto c : s) {
-            freq[c]++;
+        for(auto ch : s){
+            freq[ch]++;
         }
 
-        //tranfer frequency to vector pair, because we need to sort frequency in descending order
+        vector<pair<int, char>> freqVec;
 
-        vector<pair<int, char>> freqVec; // int ko char ke pehle isliye liya kyuki descending me krna tha
-                                         // using first element easy hai, compared to second element
-
-        for (auto num : freq) {
+        for(auto num : freq){
             freqVec.push_back({num.second, num.first});
         }
 
-        //***NEW FUNCTION GREATER IS USED TO SORT IN DESCENDING ORDER***
-        sort(freqVec.begin(), freqVec.end(), greater<pair<int, char>>());
+        sort(freqVec.begin(), freqVec.end(), greater<pair<int, char>>() );
 
-        // construct result
         string result;
-
-        for (auto c : freqVec) {
-            result.append(c.first, c.second);// pehla paramter frequency hai, dusra character
-                                             // ex : (3,a) "aaa" 
+        for(auto ch : freqVec){
+            result.append(ch.first, ch.second);
         }
+
         return result;
     }
 };
