b232ef0ae4228fb5c5c48d563af35dd2ef3ea35f
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2024-09-05 20:16:55 +0530

    Time: 8 ms (63.5%), Space: 8.8 MB (53.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()){
+        if(s.length() != t.length()){
             return false;
         }
-        // count frequency of alphabets present in str1, then minus freq for all alphabets in str2
-        // and check if array becomes 0
+
         int freq[26] = {0};
 
-        for(int i=0; i<s.size(); i++){
-            freq[s[i]-'a']++;
+        for(int i=0; i<s.length(); i++){
+            freq[ s[i] - 'a' ]++;
         }
 
-        for(int i=0; i<t.size(); i++){
-            freq[t[i]-'a']--;
+        for(int i=0; i<t.length(); i++){
+            freq[ t[i] - 'a' ]--;
         }
+
         for(int i=0; i<26; i++){
-            if (freq[i] != 0) return false;
+            if(freq[i] != 0) return false;
         }
 
         return true;
