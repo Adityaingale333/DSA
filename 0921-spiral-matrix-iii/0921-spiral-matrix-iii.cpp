b485e1f2eaf1a88fb4fa3b8c3695807ff346565c
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2024-09-11 00:14:14 +0530

    Time: 7 ms (86.25%), Space: 14 MB (18.1%) - LeetHub

--- 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
+++ 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        vector<vector<int>> directions = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+                                        //  east  south   west    north
+        vector<vector<int>> directions = { {0,1}, {1,0}, {0,-1}, {-1,0} }; 
 
         vector<vector<int>> result;
 
-        int steps = 0;
-        int dir = 0;
+        int steps = 0;    // steps to inc in one row or coloumn
+        int dir = 0;  // dir to go in 
 
         result.push_back({rStart, cStart});
 
-        while(result.size() < rows*cols){
+        while(result.size() < rows*cols){   // loop goes till result matrix is not full
             if(dir==0 || dir==2){
-                steps++;
+                steps++;            // only inc steps when it is east or west, observation 
             }
 
-            for(int count=0; count<steps; count++){
+            for(int count=0; count<steps; count++){   // use count for no of steps to go with
                 rStart += directions[dir][0];
                 cStart += directions[dir][1];
 
                 if(rStart>=0 && rStart<rows && cStart>=0 && cStart<cols){
-                    result.push_back({rStart, cStart});
+                    result.push_back({rStart, cStart});    // only push when valid
                 }
             }
-            dir = (dir+1)%4;
+            dir = (dir+1)%4;    
         }
         return result;
     }
