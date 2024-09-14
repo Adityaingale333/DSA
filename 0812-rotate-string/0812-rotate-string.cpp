edba6b72afe919adff1f5d5575f1dc5d503decbf
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2024-09-14 23:53:33 +0530

    Time: 0 ms (100%), Space: 7.7 MB (40.15%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        
-        return (s.length()==goal.length() && (s+s).contains(goal));
-
+        return ((s+s).contains(goal) && s.length()==goal.length());
     }
 };
