d97d7062356f245324a243b4aec702df9c15762a
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2024-10-15 23:07:34 +0530

    Time: 0 ms (100%), Space: 9.8 MB (12.27%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
-
-    int splitGivenSum(vector<int>& arr, int mid){
-        int n = arr.size();
-        int sum = 0, count = 0;
-        for(int i=0; i<n; i++){
-            sum += arr[i];
+    int splitGivenSum(vector<int> nums, int mid){
+        int count = 1;
+        int sum = 0;
+        for(int i=0; i<nums.size(); i++){
+            sum = sum + nums[i];
             if(sum > mid){
                 count++;
-                sum = arr[i];
+                sum = nums[i];
             }
         }
-        if(sum!=0) count++;
         return count;
     }
-
     int splitArray(vector<int>& nums, int k) {
-     int n = nums.size();
-     int largest = INT_MIN, tsum = 0;
-     for(int i=0;i<n; i++){
-        largest = max(largest, nums[i]);
-        tsum = tsum + nums[i];
-     }
+        int n = nums.size();
 
-    if(n<k) return -1;
+        if(n<k) return -1;
 
-     int low = largest, high = tsum;
-     while(low<=high){
-        int mid = (low+high)/2;
-        int split = splitGivenSum(nums, mid);
-        if(split <= k){
-            high = mid-1;
+        int largest = INT_MIN;
+        int tsum = 0;
+        for(int i=0; i<n; i++){
+            largest = max(largest, nums[i]);
+            tsum = tsum + nums[i];
         }
-        else{
-            low = mid+1;
+
+        int low = largest, high = tsum;
+        int ans = 0;
+        while(low <= high){
+            int mid = (low+high)/2;
+            int split = splitGivenSum(nums, mid);
+            if(split <= k){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
         }
-     }
-     return low;   
+        return ans;
     }
 };
