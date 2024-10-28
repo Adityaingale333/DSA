2ca3962273f330d67efcc7fe58d549f5811c5bd0
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2024-10-28 17:01:07 +0530

    Time: 7 ms (36.93%), Space: 11.2 MB (54.93%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        int n = s.size();
-
         unordered_map<char, int> freq;
 
         for(auto ch : s){
             freq[ch]++;
-        }
+        } 
 
-        vector<pair<int, char>> freqVec;
+        vector <pair<int,char>> freqVec;
 
         for(auto num : freq){
-            freqVec.push_back({num.second, num.first});
+           freqVec.push_back({num.second, num.first}); 
         }
 
-        sort(freqVec.begin(), freqVec.end(), greater<pair<int, char>>() );
+        sort(freqVec.begin(), freqVec.end(), greater<pair<int,char>> () );
 
-        string result;
+        string result ;
+
         for(auto ch : freqVec){
             result.append(ch.first, ch.second);
         }
-
         return result;
     }
 };
