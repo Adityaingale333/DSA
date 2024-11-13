7ca3a5705fba021b6eca2cfbbd8ee2d200530f16
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2024-11-13 17:41:57 +0530

    Time: 5 ms (80.34%), Space: 17.8 MB (74.65%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -31,18 +31,18 @@ public:
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> prefixSum(n,vector<int>(m));
-        for(int j=0; j<m; j++){
-            int sum = 0;
-            for(int i=0; i<n; i++){
-                sum = sum + matrix[i][j] - '0' ;
-                if(matrix[i][j] - '0' == 0) sum = 0;
-                prefixSum[i][j] = sum;
-            }
-        }
+
+        vector<int> heights(m,0);
         int area = 0;
+
         for(int i=0; i<n; i++){
-            area = max(area, largestRectangleArea(prefixSum[i]));
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == '1'){
+                    heights[j] = heights[j]+1;
+                }
+                else heights[j] = 0;
+            }
+            area = max(area, largestRectangleArea(heights));
         }
         return area;
     }
