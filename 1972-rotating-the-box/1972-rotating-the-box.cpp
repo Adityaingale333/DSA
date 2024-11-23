22a40640635ef38099ebeed6e228241ffd151cb1
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2024-11-23 22:26:54 +0530

    Time: 181 ms (86.67%), Space: 56.4 MB (63.41%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.cpp
+++ 1972-rotating-the-box/1972-rotating-the-box.cpp
@@ -6,35 +6,31 @@ public:
 
         vector<vector<char>> result(n, vector<char>(m));
 
+        // transpose of box
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 result[i][j] = box[j][i];
             }
         }
 
+        // rotate the box (reverse the transpose box)
         for(vector<char>& row:result){
             reverse(begin(row), end(row));
         }
 
+        // apply the gravity logic
         for(int j=0; j<m; j++){
+            int spaceBottomRow = n-1;
             for(int i=n-1; i>=0; i--){
-                if(result[i][j] == '.'){
-                    int stoneRow = -1;
+                if(result[i][j] == '*'){
+                    spaceBottomRow = i-1;
+                    continue;
+                } 
 
-                    for(int k=i-1; k>=0; k--){
-                        if(result[k][j] == '*'){
-                            break;
-                        }
-                        else if(result[k][j] == '#'){
-                            stoneRow = k;
-                            break;
-                        }
-                    }
-
-                    if(stoneRow != -1){
-                        result[i][j] = '#';
-                        result[stoneRow][j] = '.';
-                    }
+                if(result[i][j] == '#'){
+                    result[i][j] = '.';
+                    result[spaceBottomRow][j] = '#';
+                    spaceBottomRow--;
                 }
             }
         }
