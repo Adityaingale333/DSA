7ba158d02230093cdb915930a9ea787e1790ffdb
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2024-12-30 23:20:07 +0530

    Time: 0 ms (100%), Space: 27.3 MB (11.75%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    int trap(vector<int>& arr) {
-        int n = arr.size();
-
-        int lMax = 0;
-        int rMax = 0;
+//                       EASY FORMULA
+//  at point i, rainwater collected will be  min(l_max[i],r_max[i]) - height[i]
+    vector<int> getLeftMax(vector<int>& height, int n){
+        vector<int> l_max(n);
+        l_max[0] = height[0];
+        for(int i=1; i<n; i++){
+            l_max[i] = max(l_max[i-1], height[i]);
+        }
+        return l_max;
+    } 
+    vector<int> getRightMax(vector<int>& height, int n){
+        vector<int> r_max(n);
+        r_max[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--){
+            r_max[i] = max(r_max[i+1], height[i]);
+        }
+        return r_max;
+    }
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> l_max = getLeftMax(height,n);
+        vector<int> r_max = getRightMax(height,n);
         int total = 0;
 
-        int l=0, r = n-1;
-
-        while(l<r){
-            if(arr[l] <= arr[r]){
-                if(lMax > arr[l]){
-                    total = total + lMax - arr[l];
-                }
-                else lMax = arr[l];
-                l++;
-            }
-            else{
-                if(rMax > arr[r]){
-                    total = total + rMax - arr[r];
-                }
-                else rMax = arr[r];
-                r--;
-            }
+        for(int i=0; i<n; i++){
+            total = total + min(l_max[i],r_max[i]) - height[i];
         }
         return total;
     }
