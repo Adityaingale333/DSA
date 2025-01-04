9a9951ee815de146deab2bd99e28178803bdf2f3
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-01-04 10:06:19 +0530

    Time: 4 ms (5.7%), Space: 30.1 MB (21.56%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,31 +2,18 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        int newk = k%n; // to wrap around
-       int low1 = 0, high1 = n-newk-1;
+        int new_k = k%n;
+        stack<int> st;
 
-       while(low1 <= high1){
-        swap(nums[low1], nums[high1]);
-        low1++;
-        high1--;
-       } 
-
-
-       int low2 = n-newk, high2 = n-1; 
-
-       while(low2 <= high2){
-        swap(nums[low2], nums[high2]);
-        low2++;
-        high2--;
-       }
-
-       int low3 = 0, high3 = n-1;
-
-       while(low3 <= high3){
-        swap(nums[low3], nums[high3]);
-        low3++;
-        high3--;
-       }
-
+        for(int i=n - new_k - 1; i>=0; i--){
+            st.push(nums[i]);
+        }
+        for(int i=n-1; i>=n - new_k; i--){
+            st.push(nums[i]);
+        }
+        for(int i=0; i<n; i++){
+            nums[i] = st.top();
+            st.pop();
+        }
     }
 };
