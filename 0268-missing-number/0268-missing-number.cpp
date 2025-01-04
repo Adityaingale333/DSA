8332a8bcc45d017fd9e753a2bc3267ab6a4b305a
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-01-04 10:58:01 +0530

    Time: 0 ms (100%), Space: 21.9 MB (27.5%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
+ // Using BIT MANIPULATION
+ // take xor from 0 to n
+ // take xor of array
+ // take resultant xor, it will cancel out all the element occuring double, leaving missing
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-
-        int sum = (n*(n+1)) / 2;
-        int arrSum = 0;
-
-        for(int i=0; i<n; i++){
-            arrSum = arrSum + nums[i];
+        int xor1 = 0;
+        int xor2 = 0;
+        for(int i=0; i<=n; i++){
+            xor1 = xor1 ^ i; 
         }
-
-        int ans = sum - arrSum;
-
+        for(int i=0; i<n; i++){
+            xor2 = xor2 ^ nums[i];
+        }
+        int ans = xor1 ^ xor2;
         return ans;
     }
 };
