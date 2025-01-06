ea98a350176cf287a71d689f3686af654c58e8e4
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-01-06 17:40:25 +0530

    Time: 0 ms (100%), Space: 10.2 MB (18.32%) - LeetHub

--- 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.cpp
+++ 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.cpp
@@ -4,19 +4,19 @@ public:
         int n = s.size();
 
         int i=0, j=0;
-        int length = 0;
         int max_len = 0;
+        int currCost = 0;
         while(j<n){
-            if(abs(s[j]-t[j]) <= maxCost ){
-                length = j-i+1;
-                max_len = max(max_len,length);
-                maxCost = maxCost - abs(s[j]-t[j]) ;
-                j++;
-            }
-            else{
-                maxCost = maxCost + abs(s[i]-t[i]);
+            currCost = currCost + abs(s[j]-t[j]);
+
+            while(currCost > maxCost){
+                currCost = currCost - abs(s[i]-t[i]);
                 i++;
             }
+
+            max_len = max(max_len, j-i+1);
+
+            j++;
         }
         return max_len;
     }
