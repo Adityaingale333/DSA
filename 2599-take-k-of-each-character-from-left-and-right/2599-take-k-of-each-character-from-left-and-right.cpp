a9211478d99893d8ff4efb659ac130452dfa1c75
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-01-09 19:10:28 +0530

    Time: 12 ms (67.55%), Space: 13.2 MB (20.72%) - LeetHub

--- 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     int takeCharacters(string s, int k) {
-       int n = s.size();
+        int n = s.size();
 
-       int counta = 0;
-       int countb = 0;
-       int countc = 0;
+        int counta = 0;
+        int countb = 0;
+        int countc = 0;
 
-        for(char &ch:s){
-            if(ch == 'a') counta++;
-            else if(ch == 'b') countb++;
+        for(int i=0; i<n; i++){
+            if(s[i]=='a') counta++;
+            else if(s[i]=='b') countb++;
             else countc++;
         }
 
         if(counta<k || countb<k || countc<k) return -1;
 
-       int i = 0;
-       int j = 0; 
-       int notDeletedWindowSize = 0;
+        int i=0, j=0;
+        int windowNotDelete = 0;
 
-       while(j < n){
-        if(s[j] == 'a') counta--;
-        else if(s[j] == 'b') countb--;
-        else countc--;
+        while(j<n){
+            if(s[j]=='a') counta--;
+            else if(s[j]=='b') countb--;
+            else countc--;
 
-        while(i<=j && (counta<k || countb<k || countc<k)){
-            if(s[i] == 'a') counta++;
-            else if(s[i] == 'b') countb++;
-            else countc++;
+            while(i<=j && (counta < k || countb < k || countc < k) ){  // a,b,c inside window should be less than k
+                if(s[i]=='a') counta++;
+                else if(s[i]=='b') countb++;
+                else countc++;
 
-            i++;
+                i++;
+            }
+            windowNotDelete = max(windowNotDelete, j-i+1);
+            j++;
         }
-
-        notDeletedWindowSize = max(notDeletedWindowSize, j-i+1);
-        j++;
-       }
-       return n - notDeletedWindowSize;
+        return n - windowNotDelete;
     }
 };
