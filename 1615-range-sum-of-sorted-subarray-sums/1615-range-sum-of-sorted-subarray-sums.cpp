91656cd0f38c58e9ff97aa686826812e1c76b4e6
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-01-30 11:07:18 +0530

    Time: 62 ms (83.91%), Space: 11.3 MB (80.75%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     int M = 1e9 + 7;
+
+    typedef pair<int,int> P; // {subarray sum, end index of this subarray}
+    
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<int> temp;
+        priority_queue<P, vector<P>, greater<P>> minh;
 
         for(int i=0; i<n; i++){
-            int sum = 0;
-            for(int j=i; j<n; j++){
-                sum = sum + nums[j];
-                temp.push_back(sum);
-            }
-        } 
-
-        sort(temp.begin(), temp.end());
-
-        int ans = 0;
-        for(int i=left-1; i<=right-1; i++){
-            ans = (ans + temp[i]) % M;
+            minh.push({nums[i], i});
         }
-        return ans;
+
+        int result = 0;
+
+        for(int i=1; i<=right; i++){
+            auto top = minh.top();
+            minh.pop();
+
+            int sum = top.first;
+            int idx = top.second;
+
+            if(i >= left){
+                result = (result + sum) % M;
+            }
+
+            int new_idx = idx + 1;
+            P new_pair;
+            if(new_idx < n){
+                new_pair.first = sum + nums[new_idx];
+                new_pair.second = new_idx;
+                minh.push(new_pair);
+            }
+        }
+        return result;
     }
 };
