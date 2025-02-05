097defc88867674c2735548a5bcb69af46fd845c
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-02-05 12:06:14 +0530

    Time: 10 ms (56.58%), Space: 11.6 MB (26.64%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,45 +1,43 @@
 class Solution {
 public:
+ /*1. Build a frequency map of t.
+   2. Use a sliding window (i and j) over s.
+   3. Expand the window to include valid characters (j++).
+   4. Shrink the window when all characters of t are matched (i++), keeping track of the smallest valid window.
+   5. Return the smallest valid substring or "" if no valid window exists.*/
     string minWindow(string s, string t) {
         int n = s.size();
+        unordered_map<char,int> mp;
 
-        if(t.size() > n) return "";
-
-        unordered_map<char, int> mp;
-        // store in map
-        for(char &ch : t){
+        for(char& ch:t){
             mp[ch]++;
         }
-        
-        int requiredCount = t.size();
-        int i=0, j=0;
-        int minWindowSize = INT_MAX;
-        int starti = 0;
 
-        while(j < n){
-            char ch = s[j];
+        int l=0, r=0;
+        int requiredCount = t.size(); // size of t, bcz we have to check that much characters only present in t
+        int startl = 0; // to get the substring from where the smallest window will start
+        int min_WindowSize = INT_MAX;
 
+        while(r<n){
+            char ch = s[r];
             if(mp[ch] > 0){
-               requiredCount--;
+                requiredCount--;
             }
-
-            mp[ch]--; 
-
-            while(requiredCount == 0){
-                int currWindowSize = j-i+1;
-                if(minWindowSize > currWindowSize){
-                    minWindowSize = currWindowSize;
-                    starti = i;
+            mp[ch]--;
+            while(requiredCount==0){
+                int windowSize = r-l+1;
+                if(min_WindowSize > windowSize){
+                    min_WindowSize = windowSize;
+                    startl = l;
                 }
-                mp[s[i]]++;
-                if(mp[s[i]] > 0){
+                mp[s[l]]++;
+                if(mp[s[l]] > 0){
                     requiredCount++;
-                }
-                i++;
+                } 
+                l++;
             }
-            j++;
+            r++;
         }
-
-        return minWindowSize == INT_MAX ? "" : s.substr(starti, minWindowSize);
+        return min_WindowSize == INT_MAX ? "" : s.substr(startl, min_WindowSize);
     }
 };
