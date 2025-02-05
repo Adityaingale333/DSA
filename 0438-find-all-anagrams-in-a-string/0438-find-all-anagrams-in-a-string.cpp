ad9ce443076a36ae0391473d10769e9589cdee39
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-02-05 22:51:19 +0530

    Time: 473 ms (6.83%), Space: 173.8 MB (5.19%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-  //  Here we are passing the map by reference bcz this makes the code efficient
-  //  and does not create a copy of the map freq every time the function is called
-  //  and to avoid te changes to be done in this function, we use "const" which does 
-  //  does not aloow changes to be made  
-    bool allZero(const unordered_map<char,int>& freq){
-        for(auto& [ch,count] :freq){
-            if(count!=0) return 0;
+    bool allZero(unordered_map<char,int> freq){
+        for(auto& [ch,count] : freq){
+            if(count != 0){
+                return 0;
+            }
         }
         return 1;
     }
@@ -14,13 +12,14 @@ public:
         int n = s.size();
         unordered_map<char,int> freq;
 
-        for(int i=0; i<p.size(); i++){
-            freq[p[i]]++;
+        for(auto& ch : p){
+            freq[ch]++;
         }
 
         int l=0, r=0;
         vector<int> ans;
-        while(r<n){
+
+        while(r < n){
             freq[s[r]]--;
             if(r-l+1 == p.size()){
                 if(allZero(freq)){
