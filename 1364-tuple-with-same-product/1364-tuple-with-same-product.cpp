064b6f3cfb5e9d774fbe84f1c5a2fcb91593dbc4
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-02-07 01:10:30 +0530

    Time: 303 ms (78.02%), Space: 85.2 MB (23.53%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    // if a*b == c*d then either a or b has to be smaller or bigger than c and d
-    // it is not possible that both a and b are smaller or larger than c and d
-    // so we sort the array and then put i at start ang j at end and then take,
-    // k=i+1 and then find the value of d and check in set if we can find the pair 
+    // Use a HashMap. The key stores the product of two numbers. 
+    // The value stores how many times that product appears.
+    // If a product appears freq times, then we can pick any two pairs having that product in
+    // (freq) * (freq-1) / 2 ways. nC2, n times appearing we want combinatio of 2.
     int tupleSameProduct(vector<int>& nums) {
         int n = nums.size();
         int tuple = 0;
 
-        sort(nums.begin(), nums.end());
-
+        unordered_map<int,int> map; // product, frequency
         for(int i=0; i<n; i++){
-            for(int j=n-1; j>i; j--){
+            for(int j=i+1; j<n; j++){
                 int product = nums[i] * nums[j];
-                unordered_set<int> st;
-                for(int k=i+1; k<j; k++){
-                    if(product % nums[k] == 0){
-                        int dvalue = product/nums[k];
-                        if(st.count(dvalue)){
-                            tuple++;
-                        }
-                        st.insert(nums[k]);
-                    }
-                }
+                map[product]++;
             }
         }
+
+        for(auto& it:map){
+            int prod = it.first;
+            int freq = it.second;
+            tuple = tuple + ( (freq)*(freq-1) )/ 2;
+        }
+
         return tuple*8 ;
     }
 };
