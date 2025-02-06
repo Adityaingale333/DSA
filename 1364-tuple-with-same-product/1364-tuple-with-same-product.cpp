1d6e7cb7cedbb23ec22d98ed04e052c6fa049d6b
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-02-07 00:37:02 +0530

    Time: 2772 ms (5.01%), Space: 492.3 MB (6.27%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    // if a*b == c*d then either a or b has to be smaller or bigger than c and d
+    // it is not possible that both a and b are smaller or larger than c and d
+    // so we sort the array and then put i at start ang j at end and then take,
+    // k=i+1 and l=j-1, also l>k to avoid duplicate  
+    int tupleSameProduct(vector<int>& nums) {
+        int n = nums.size();
+        int tuple = 0;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i=0; i<n; i++){
+            for(int j=n-1; j>i; j--){
+                int product = nums[i] * nums[j];
+                unordered_set<int> st;
+                for(int k=i+1; k<j; k++){
+                    if(product % nums[k] == 0){
+                        int dvalue = product/nums[k];
+                        if(st.count(dvalue)){
+                            tuple++;
+                        }
+                        st.insert(nums[k]);
+                    }
+                }
+            }
+        }
+        return tuple*8 ;
+    }
+};
