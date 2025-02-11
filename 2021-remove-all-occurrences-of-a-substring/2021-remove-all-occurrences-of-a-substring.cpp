acb50cc1436b069f2cc943e2b765041f3b0e4b2b
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-02-11 17:38:47 +0530

    Time: 11 ms (5.85%), Space: 20.8 MB (5.32%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while(s.length()!=0 && s.find(part) < s.length()){
-            s.erase(s.find(part), part.length());
-        } 
-        return s;
+        int n = s.size();
+        string result = "";
+        for(int i=0; i<n; i++){
+            result = result + s[i];
+            // s.substr(x) then substring will be from x -> end of string.
+            if(result.size() >= part.size() && result.substr(result.size()-part.size()) == part){
+                result.erase(result.length() - part.size());
+            }
+        }
+        return result;
     }
 };
