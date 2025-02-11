1282ec4b0c0123f376b22a30fc295033596f082f
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-02-11 17:52:53 +0530

    Time: 19 ms (5.85%), Space: 29.2 MB (5.32%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,15 +1,39 @@
 class Solution {
 public:
+    bool check(stack<char> st, string part, int n){
+        for(int i=n-1; i>=0; i--){
+            if(st.top() != part[i]){
+                return false;
+            }
+            st.pop();
+        }
+        return true;
+    }
+
     string removeOccurrences(string s, string part) {
         int n = s.size();
-        string result = "";
+        int m = part.size();
+
+        stack<char> st;
+
         for(int i=0; i<n; i++){
-            result = result + s[i];
-            // s.substr(x) then substring will be from x -> end of string.
-            if(result.size() >= part.size() && result.substr(result.size()-part.size()) == part){
-                result.erase(result.length() - part.size());
+            st.push(s[i]);
+
+            if(st.size() >= m && check(st, part, m) == true){
+                for(int j=0; j<m; j++){
+                    st.pop();
+                }
             }
         }
-        return result;
+
+        string ans = "";
+        while(!st.empty()){
+            ans = ans + st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
     }
 };
