c3154731d5441ea5a4e6278da8b01f30142e34af
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-02-18 19:31:28 +0530

    Time: 0 ms (100%), Space: 8 MB (63.49%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    bool matches_pattern(string num, string pattern){
-        for(int i=0; i<pattern.size(); i++){
-            if(pattern[i] == 'I' && num[i] > num[i+1] ||
-               pattern[i] == 'D' && num[i] < num[i+1] ){
-                
-                return false;
-
-            }
-        }
-        return true;
-    }
     string smallestNumber(string pattern) {
         int n = pattern.size();
+        stack<char> st;
 
+        int count = 1;
         string num = "";
-        for(int i=1; i<=n+1; i++){
-            num.push_back(i + '0');
-        }
+        for(int i=0; i<=n; i++){ // Loop goes to n (not n-1) to handle last number
+            st.push(count + '0');
 
-        while(!matches_pattern(num, pattern)){
-            next_permutation(num.begin(), num.end());
-        }
+            // If pattern[i] is 'I' or end of pattern, empty the stack
+            if(i == n || pattern[i] == 'I'){
+                while(!st.empty()){
+                    num.push_back(st.top());
+                    st.pop();
+                }
+            }
 
+            count++; // only inc count if it is D and then empty it when I occurs
+        }
         return num;
     }
 };
