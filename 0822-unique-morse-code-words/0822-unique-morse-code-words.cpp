02174c58a6efab29f874f520d5cb6d7bf37d253e
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-02-18 23:45:11 +0530

    Time: 4 ms (8.01%), Space: 11.3 MB (92.32%) - LeetHub

--- 0822-unique-morse-code-words/0822-unique-morse-code-words.cpp
+++ 0822-unique-morse-code-words/0822-unique-morse-code-words.cpp
@@ -3,16 +3,11 @@ public:
     int uniqueMorseRepresentations(vector<string>& words) {
         int n = words.size();
 
-        unordered_map<char, string> mp;
         unordered_set<string> st;
 
         vector<string> code = {".-","-...","-.-.","-..",".","..-.","--.",
         "....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
         "...","-","..-","...-",".--","-..-","-.--","--.."} ;
-
-        for(int i ='a'; i<='z'; i++){
-            mp[i] = code[i-'a'];
-        }
 
         for(auto& word : words){
             string transformation = "";
