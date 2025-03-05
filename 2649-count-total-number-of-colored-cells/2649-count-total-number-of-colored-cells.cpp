9b8552011609359af640b20cf7e5052c9a3f1cd7
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-03-05 17:20:43 +0530

    Time: 0 ms (100%), Space: 7.9 MB (61.42%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
+// using mathematical relation and pattern recognizing
+// n = 1 -> 1
+// n = 2 -> 1 + 4(2-1)
+// n = 3 -> 1 + 4(2-1) + 4(3-1)
+// n = 4 -> 1 + 4(2-1) + 4(3-1) + 4(4-1)
+// ....... taking 4 common -> 1 + 4[sum of n-1 natural numbers]
+// -> 1 + 4* { (n-1)(n-1+1)/2 }   ->  1 + 2*{(n-1)(n)}   
     long long coloredCells(int n) {
-        long long cells = 1;
-        long long time = 1;
-
-        while(time <= n){
-            cells = cells + 4*(time-1);
-            time++;
-        }
-
-        return cells;
+        return 1 + 2*( (long long)(n-1)*(n) );
     }
 };
