aa084d97b44e9208cfaffe74e8f6d6b8c8c3922e
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-03-11 20:21:14 +0530

    Time: 3 ms (90.05%), Space: 11.1 MB (35.03%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
+// We maintain a frequency vector mp(3,0) that keeps track of the counts of 'a', 'b', and 'c' in the current window.
+// Two pointers (i and j):
+// j expands the window by including new characters from s.
+// i contracts the window to count valid substrings.
+// When we find a substring that contains at least one 'a', 'b', and 'c':
+// All substrings that start at i and end at j, j+1, ..., n-1 are valid (since increasing j further keeps the substring valid).
+// We add (n - j) to result because all substrings from i to j, j+1, ..., n-1 are counted.
+// We move i forward to continue counting more possible substrings.
     int numberOfSubstrings(string s) {
-       int n = s.size();
-       int lastseen[3] = {-1,-1,-1};
-       int ans = 0;
+        int n = s.length();
+        int result = 0;
 
-       for(int i=0; i<n; i++){
-        lastseen[s[i]-'a'] = i;
-        if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1){
-            ans = ans + min(lastseen[0],min(lastseen[1],lastseen[2])) + 1;
+        vector<int> mp(3,0); // a->0, b->1, c->2
+
+        int i = 0;
+        int j = 0;
+
+        while(j<n){
+            char ch = s[j];
+            mp[ch-'a']++;
+
+            while(mp[0] > 0 && mp[1] > 0 && mp[2] > 0){
+                result = result + (n-j);
+
+                mp[s[i]-'a']--;
+                i++;
+            }
+            j++;
         }
-       } 
-       return ans; 
+        return result;
     }
 };
