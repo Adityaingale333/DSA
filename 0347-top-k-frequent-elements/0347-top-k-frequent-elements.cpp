9b0e4aab20190e63f3bedc1899412132d8d7e953
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-04-09 23:13:53 +0530

    Time: 2 ms (59.77%), Space: 18 MB (47.46%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    //               BUCKET SORT
     vector<int> topKFrequent(vector<int>& nums, int k) {
         int n = nums.size();
+        using pi = pair<int,int>;
 
-        unordered_map<int,int> mp;
-        for(auto& it : nums){
+        unordered_map<int, int> mp; // element -> freq
+        priority_queue<pi, vector<pi>, greater<pi>> minh;
+
+        for(auto& it:nums){
             mp[it]++;
         }
 
-        vector<vector<int>> bucket(n+1);
-        // index = frequency
-        // value = elements
-        // bucket[i] = elements having (i) frequency
-        for(auto& it : mp){
-            int element = it.first;
-            int freq = it.second;
-
-            bucket[freq].push_back(element);
-        }
-
-        // result
-        vector<int> ans;
-        // pick element from right to left
-        for(int i=n; i>=0; i--){
-            if(bucket[i].size() == 0) continue;
-
-            while(bucket[i].size() > 0 && k > 0){
-                ans.push_back(bucket[i].back());
-                bucket[i].pop_back();
-                k--;
+        for(auto& it:mp){
+            minh.push({it.second, it.first}); //we want the heap to sort acc to freq, so freq is pushed first 
+                                              // minh -> (freq, element)
+            if(minh.size() > k){
+                minh.pop();
             }
         }
+
+        vector<int> ans;
+        while(!minh.empty()){
+            ans.push_back(minh.top().second); // element is stored in second in minh, so we push it in ans
+            minh.pop();
+        } 
         return ans;
     }
 };
