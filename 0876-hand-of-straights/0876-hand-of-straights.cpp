3f8964c463d1f4a6aa1d513f0dd32d5b69355dc8
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-04-09 22:56:58 +0530

    Time: 47 ms (24.61%), Space: 32.4 MB (38.05%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
+
+// Use a map (which is always sorted) to always start grouping from the smallest available card.
+// For each group of size groupSize, check that all consecutive numbers curr, curr+1, ..., curr+groupSize-1 exist.
+// If any card is missing during a group build, return false.
+// Remove cards as you use them. If all can be grouped, return true.
+
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         int n = hand.size();
+        
+        if(n % groupSize != 0) return false;
 
-        if(n % groupSize != 0){
-            return false;
-        }
-
-        map<int,int> freq;
-
+        map<int, int> freq;
         for(int i=0; i<n; i++){
             freq[hand[i]]++;
         }
 
         while(!freq.empty()){
-            int curr = freq.begin()->first;
+            int curr = freq.begin() -> first;
 
             for(int i=0; i<groupSize; i++){
                 if(freq[curr + i] == 0){
