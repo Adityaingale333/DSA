456d9da07af7f53ddf19b03c66573b9cb0458811
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-04-12 00:01:14 +0530

    Time: 4 ms (91.74%), Space: 9.4 MB (90.63%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
@@ -4,27 +4,18 @@ public:
         int count = 0;
 
         for(int i=low; i<=high; i++){
-            string s = to_string(i);
-
-            int l = s.length();
-
-            if(l%2 != 0){
-                continue;
-            }
-
-            int leftSum = 0;
-            int rightSum = 0;
-
-            for(int j=0; j<l/2; j++){
-                leftSum += s[j] - '0';
-            }
-
-            for(int j=l/2; j<l; j++){
-                rightSum += s[j] - '0';
-            }
-
-            if(leftSum == rightSum){
+            if(10<=i && i<=99 && i%11 == 0){
                 count++;
+            }
+            else if(1000<=i && i<=9999){
+                int first = i/1000;
+                int second = (i/100) % 10;
+                int third = (i/10) % 10;
+                int fourth = i % 10;
+
+                if( (first + second) == (third + fourth) ){
+                    count++;
+                }
             }
         }
         return count;
