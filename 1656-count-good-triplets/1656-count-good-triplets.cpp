c6163778f912e42d33dd110e8d02ed2fd439e6a4
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-04-14 11:43:17 +0530

    Time: 10 ms (36.18%), Space: 11.1 MB (43.88%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.cpp
+++ 1656-count-good-triplets/1656-count-good-triplets.cpp
@@ -6,11 +6,12 @@ public:
 
         for(int i=0; i<n-2; i++){
             for(int j=i+1; j<n-1; j++){
-                for(int k=j+1; k<n; k++){
-                    if(abs(arr[i] - arr[j]) <= a &&
-                       abs(arr[j] - arr[k]) <= b &&
-                       abs(arr[k] - arr[i]) <= c ){
-                            count++;
+                if(abs(arr[i] - arr[j]) <= a){
+                    for(int k=j+1; k<n; k++){
+                        if(abs(arr[j] - arr[k]) <= b &&
+                            abs(arr[k] - arr[i]) <= c ){
+                                 count++;
+                        }
                     }
                 }
             }
