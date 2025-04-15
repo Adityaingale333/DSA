2c79236938596156f111b309d1e0a9baf7657f91
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-04-15 11:27:29 +0530

    Time: 0 ms (100%), Space: 8.4 MB (70.26%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -1,44 +1,50 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        int n = s.size();
-        vector<int> count(26,0);
+        int n = s.length();
 
-        int maxFreq = 0;
-        int maxFreqChar;
-        for(auto& ch : s){
-            count[ch-'a']++;
-
-            if(count[ch-'a'] > maxFreq){
-                maxFreq = count[ch-'a'];
-                maxFreqChar = ch;
-            }
-
-            if(count[ch-'a'] > (n+1)/2){
+        vector<int> freq(26,0); // store the frequency of all char
+        for(int i=0; i<n; i++){
+            freq[s[i] - 'a']++;
+// *** if any char appears more than half the size then it's not possible to construct the result
+            if(freq[s[i]-'a'] > (n+1)/2){ 
                 return "";
             }
         }
 
-        string ans = s;
-        int i = 0;
-
-        while(count[maxFreqChar - 'a'] > 0){
-            ans[i] = maxFreqChar;
-            i = i+2;
-            count[maxFreqChar - 'a']--;
-        }
-
-        for(char ch='a'; ch<='z'; ch++){
-            while(count[ch-'a'] > 0){
-                if(i >= n){
-                    i = 1;
-                }
-
-                ans[i] = ch;
-                i = i+2 ;
-                count[ch-'a']--;
+// to get the freq in required order we are using pq 
+        priority_queue<pair<int, char>> maxh;
+        for(int i=0; i<26; i++){
+            if(freq[i] > 0){
+                maxh.push({freq[i], i+'a'});
             }
         }
-        return ans;
+
+        string result = "";
+        while(maxh.size() >= 2){  // doing this bcz if the size is 1, then we do not need to visit pq while loop
+                                // we can directly pop it using the below if statement 
+            auto temp1 = maxh.top();
+            maxh.pop();             // since we are checking the size >= 2, means it safer and faster
+                                    // to pop 2 elements each time
+            auto temp2 = maxh.top();
+            maxh.pop();
+
+            result.push_back(temp1.second);
+            temp1.first--;
+            result.push_back(temp2.second);
+            temp2.first--;
+
+            if(temp1.first > 0){
+                maxh.push(temp1);
+            }
+            if(temp2.first > 0){
+                maxh.push(temp2);
+            }
+        }
+        if(!maxh.empty()){ // if it contains only single element, then we can simply use it to save time
+            result.push_back(maxh.top().second);
+            maxh.pop();
+        }
+        return result;
     }
 };
