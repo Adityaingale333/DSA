1bc5325cddf6211367219dac5d4cba485d46814f
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-04-18 15:16:18 +0530

    Time: 0 ms (100%), Space: 40.1 MB (94.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,29 +2,18 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> mulForward(n,0);
-        vector<int> mulBackward(n,0);
-        int mulf = 1;
-
-        for(int i=0; i<n; i++){
-            mulf = mulf * nums[i];
-            mulForward[i] = mulf;
-        }
-
-        int mulb = 1;
-        for(int i=n-1; i>=0; i--){
-            mulb = mulb * nums[i];
-            mulBackward[i] = mulb;
-        }
-
-        int j = 1;
         vector<int> ans(n,0);
-        ans[0] = mulBackward[1];
-        ans[n-1] = mulForward[n-2];
-        while(j < n-1){
-            int val = mulForward[j-1] * mulBackward[j+1];
-            ans[j] = val;
-            j++;
+
+        int left = 1;
+        for(int i=0; i<n; i++){
+            ans[i] = left;
+            left = left*nums[i];
+        }
+
+        int right = 1;
+        for(int i=n-1; i>=0; i--){
+            ans[i] = ans[i]*right;
+            right = right*nums[i];
         }
         return ans;
     }
