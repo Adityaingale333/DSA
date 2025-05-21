1e43556173d96ebd3bdb8dd8ec8b1e9acada6a01
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-05-22 00:12:50 +0530

    Time: 1 ms (16.41%), Space: 18.8 MB (12.91%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,57 +3,25 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        bool firstRowImpacted = false;
-        bool firstColImpacted = false;
 
-        // check first row impacted hai ya nahi
-        for(int col=0; col<n; col++){
-            if(matrix[0][col] == 0){
-                firstRowImpacted = true;
-                break;
-            }
-        }
+        set<int> rows;
+        set<int> cols;
 
-        // check first col impacted hai ya nahi
-        for(int row=0; row<m; row++){
-            if(matrix[row][0] == 0){
-                firstColImpacted = true;
-                break;
-            }
-        }
-        
-        // set markers in the first row/col in matrix
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(matrix[i][j] == 0){
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+                    rows.insert(i);
+                    cols.insert(j);
                 }
             }
         }
 
-        // using markers set row and col elements as zero
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(rows.find(i) != rows.end() || cols.find(j) != cols.end()){
                     matrix[i][j] = 0;
                 }
             }
         }
-
-        // check for affected row/col and make it zero if required
-        if(firstRowImpacted){
-            for(int col=0; col<n; col++){
-                matrix[0][col] = 0;
-            }
-        }
-
-        // check for affected row/col and make it zero if required
-        if(firstColImpacted){
-            for(int row=0; row<m; row++){
-                matrix[row][0] = 0;
-            }
-        }
-        
     }
 };
