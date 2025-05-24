8a265d313753a79cdff49fabcfd26c09973f19fa
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-05-24 19:46:35 +0530

    Time: 0 ms (100%), Space: 10 MB (97.15%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> t(n+1, 0); // t[i] -> max stolen money till house i
-        t[0] = 0; // 0 house me chori krne ke baad, obv 0 money mila hoga
-        t[1] = nums[0]; // 1st house me chori krne ke baad, uske pass kitne money hai
+
+        int prev2 = 0; // this is t[0] , 0 house me chori krne ke baad, obv 0 money mila hoga
+        int prev = nums[0]; //this is t[1], 1st house me chori krne ke baad, uske pass kitne money hai
 
         for(int i=2; i<=n; i++){
-            int pick = nums[i-1] + t[i-2];
-            int skip = t[i-1];
-            t[i] = max(pick, skip);
+            int pick = nums[i-1] + prev2;
+            int skip = prev;
+            int curri = max(pick, skip);
+
+            prev2 = prev;
+            prev = curri;
         } 
-        return t[n];
+        return prev;
     }
 };
