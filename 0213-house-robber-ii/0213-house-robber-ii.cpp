f1d02c30ee48bef1bfb4d512c9c27e1d07f9c8e5
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-05-24 20:29:17 +0530

    Time: 0 ms (100%), Space: 10.7 MB (63.32%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int solve(int start, int end, vector<int>& nums,  vector<int>& t){
-        if(end < start){
-            return 0;
+        int len = end - start + 1; // total houses we are considering, n houses
+        t[0] = 0; // after robbing 0 house
+        t[1] = nums[start]; // after robbing only startting house
+
+        for(int i=2; i<=len; i++){ // end+1 -> if n elements then total n+1 index, n-2 elements -> end+1 -> n-1 index
+            int pick = nums[start + i - 1] + t[i-2]; // start is added to take account of correct house
+            int skip = t[i-1];
+            t[i] = max(pick, skip);
         }
-
-        if(t[end] != -1){
-            return t[end];
-        }
-
-        int take = nums[end] + solve(start, end-2, nums, t);
-        int skip = 0 + solve(start, end-1, nums, t);
-
-        return t[end] = max(take, skip);
+        return t[len];
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
@@ -20,8 +18,8 @@ public:
         if(n == 1) return nums[0];
         if(n == 2) return max(nums[0], nums[1]);
 
-        vector<int> t1(n+1, -1);
-        vector<int> t2(n+1, -1);
+        vector<int> t1(n+1, 0);
+        vector<int> t2(n+1, 0);
 
         int takeZero = solve(0, n-2, nums, t1); // we rob house no.(0) means we can not rob last(n-1) house, rob (n-2) house
         int skipZero = solve(1, n-1, nums, t2); // we skip house no.(0) means we can rob the last house 
