5c03a225e3951fd3bbdffba4fa465eb6699e55cf
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-05-24 20:31:14 +0530

    Time: 0 ms (100%), Space: 10.1 MB (89.42%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    int solve(int start, int end, vector<int>& nums,  vector<int>& t){
+    int solve(int start, int end, vector<int>& nums){
         int len = end - start + 1; // total houses we are considering, n houses
-        t[0] = 0; // after robbing 0 house
-        t[1] = nums[start]; // after robbing only startting house
+        int prev2 = 0; //this is t[0], after robbing 0 house
+        int prev = nums[start]; //this is t[1], after robbing only startting house
 
         for(int i=2; i<=len; i++){ // end+1 -> if n elements then total n+1 index, n-2 elements -> end+1 -> n-1 index
-            int pick = nums[start + i - 1] + t[i-2]; // start is added to take account of correct house
-            int skip = t[i-1];
-            t[i] = max(pick, skip);
+            int pick = nums[start + i - 1] + prev2; // start is added to take account of correct house
+            int skip = prev;
+            int curri = max(pick, skip);
+
+            prev2 = prev;
+            prev = curri;
         }
-        return t[len];
+        return prev;
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
@@ -18,11 +21,8 @@ public:
         if(n == 1) return nums[0];
         if(n == 2) return max(nums[0], nums[1]);
 
-        vector<int> t1(n+1, 0);
-        vector<int> t2(n+1, 0);
-
-        int takeZero = solve(0, n-2, nums, t1); // we rob house no.(0) means we can not rob last(n-1) house, rob (n-2) house
-        int skipZero = solve(1, n-1, nums, t2); // we skip house no.(0) means we can rob the last house 
+        int takeZero = solve(0, n-2, nums); // we rob house no.(0) means we can not rob last(n-1) house, rob (n-2) house
+        int skipZero = solve(1, n-1, nums); // we skip house no.(0) means we can rob the last house 
 
         return max(takeZero, skipZero);
     }
