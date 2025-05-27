bb2b83db34b1772af155afb0af3b175c54afa2dd
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-05-27 21:34:44 +0530

    Time: 196 ms (35.51%), Space: 11.7 MB (74.01%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    // BASE CASE to handle 0's in the array
+        // if(s1 == 0 && nums[0] == 0) return 2;
+    // When both the target sum s1 is 0 and arr[0] is also 0:
+    // You have two valid subsets: One that includes the zero , One that excludes the zero
+    //  Both lead to sum = 0, so count = 2.
+        //  if(s1 == 0 && s1 == nums[0]) return 1;
+    // If either:
+    // s1 == 0: You can form a subset (the empty subset) without using any element → 1 valid way
+    // s1 == arr[0]: You can pick only this one element to form the subset → 1 valid way
+    // So return 1.
+        // return 0;
+    // If none of the above conditions are satisfied:
+    // You cannot form s1 using only arr[0].
+    // So return 0.
+    int solve(int i, int s1, vector<int>& nums){
+        if(i == 0){
+            if(s1 == 0 && nums[0] == 0) return 2;
+            if(s1 == 0 || s1 == nums[0]) return 1;
+            return 0;
+        }
+
+        int notTake = solve(i-1, s1, nums);
+
+        int take = 0;
+        if(nums[i] <= s1){
+            take = solve(i-1, s1-nums[i], nums);
+        }
+        return take + notTake;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += nums[i];
+        }
+
+        // s1 - s2 = target -> s1 + s2 = sum
+        // s1 - (sum - s1) = target -> s1 = (target + sum)/2
+        int s1 = (target + sum)/2;
+
+        if(sum < target || (target+sum)%2 != 0) return 0;
+        
+        return solve(n-1, s1, nums);
+    }
+};
