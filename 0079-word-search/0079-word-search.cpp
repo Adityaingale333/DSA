29e695ce20c6ed46660c4908f225be0b14184bf3
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-05-27 09:01:32 +0530

    Time: 888 ms (15.56%), Space: 11.6 MB (47.61%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,43 +1,56 @@
 class Solution {
 public:
-    int m;
-    int n;
-    vector<vector<int>> directions{{1,0}, {-1,0}, {0,1}, {0,-1}};
-    bool find(int i, int j, vector<vector<char>>& board, string& word, int idx){
+    bool solve(int i, int j, vector<vector<char>>& board, int idx, string word){
         if(idx == word.length()){
             return true;
         }
 
-        if(i<0 || i>=m || j<0 || j>=n || board[i][j] == '$'){
+        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] == '$'){
             return false;
         }
 
         if(board[i][j] != word[idx]) return false;
 
-        char ch = board[i][j];
+        char ch = board[i][j]; 
         board[i][j] = '$';
 
-        for(auto& dir : directions){
-            int new_i = i + dir[0];
-            int new_j = j + dir[1];
-            if(find(new_i, new_j, board, word, idx+1)){
-                return true;
-            }
+        if(solve(i-1, j, board, idx+1, word)){
+            return true;
         }
+
+        if(solve(i, j+1, board, idx+1, word)){
+            return true;
+        }
+
+        if(solve(i, j-1, board, idx+1, word)){
+            return true;
+        }
+
+        if(solve(i+1, j, board, idx+1, word)){
+            return true;
+        }
+        
         board[i][j] = ch;
+
         return false;
 
     }
     bool exist(vector<vector<char>>& board, string word) {
-        m = board.size();
-        n = board[0].size();
+        int m = board.size();
+        int n = board[0].size();
+
+        string temp = "";
+        bool isPossible = false;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(board[i][j] == word[0] && find(i, j, board, word, 0)){
-                    return true;
+                if(board[i][j] == word[0]){
+                    isPossible = solve(i, j, board, 0, word);
+                    if(isPossible){
+                        return true;
+                    }
                 }
             }
         }
-        return false;
+        return isPossible;
     }
 };
