d6f520fc87583e7246c149a98b6fedab186712d2
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-05-27 23:34:10 +0530

    Time: 0 ms (100%), Space: 8.5 MB (4.56%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int sum1 = 0;
-        int sum2 = 0;
-        for(int i=1; i<=n; i++){
-            if( (i % m) != 0){
-                sum1 += i;
-            }
-            else{
-                sum2 += i;
-            }
-        }
-        return sum1 - sum2;
+        int k = n / m;
+        return n * (n + 1) / 2 - k * (k + 1) * m;
     }
 };
