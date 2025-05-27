eacecb13e64bf3e2432f1fc4be2b08c690cb1a45
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-05-27 21:58:23 +0530

    Time: 4 ms (80.69%), Space: 15.6 MB (45.32%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,38 +1,5 @@
 class Solution {
-public:
-    // BASE CASE to handle 0's in the array
-        // if(s1 == 0 && nums[0] == 0) return 2;
-    // When both the target sum s1 is 0 and arr[0] is also 0:
-    // You have two valid subsets: One that includes the zero , One that excludes the zero
-    //  Both lead to sum = 0, so count = 2.
-        //  if(s1 == 0 && s1 == nums[0]) return 1;
-    // If either:
-    // s1 == 0: You can form a subset (the empty subset) without using any element → 1 valid way
-    // s1 == arr[0]: You can pick only this one element to form the subset → 1 valid way
-    // So return 1.
-        // return 0;
-    // If none of the above conditions are satisfied:
-    // You cannot form s1 using only arr[0].
-    // So return 0.
-    int solve(int i, int s1, vector<int>& nums, vector<vector<int>>& t){
-        if(i == 0){
-            if(s1 == 0 && nums[0] == 0) return 2;
-            if(s1 == 0 || s1 == nums[0]) return 1;
-            return 0;
-        }
-
-        if(t[i][s1] != -1){
-            return t[i][s1];
-        }
-
-        int notTake = solve(i-1, s1, nums, t);
-
-        int take = 0;
-        if(nums[i] <= s1){
-            take = solve(i-1, s1-nums[i], nums, t);
-        }
-        return t[i][s1] = take + notTake;
-    }
+public: 
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int sum = 0;
@@ -46,8 +13,20 @@ public:
 
         if(sum < abs(target) || (target+sum)%2 != 0) return 0;
 
-        vector<vector<int>> t(n, vector<int>(s1+1, -1));
+        vector<vector<int>> t(n+1, vector<int>(s1+1, 0));
 
-        return solve(n-1, s1, nums, t);
+        t[0][0] = 1;
+
+        for(int i=1; i<n+1; i++){
+            for(int j=0; j<s1+1; j++){
+                if(nums[i-1] <= j){
+                    t[i][j] = t[i-1][j-nums[i-1]] + t[i-1][j];
+                }
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        return t[n][s1];
     }
 };
