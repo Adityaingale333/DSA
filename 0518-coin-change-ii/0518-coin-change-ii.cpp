94b334dade23196c05b6cfd54fe137317ad8d747
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-05-28 19:43:26 +0530

    Time: 24 ms (41.13%), Space: 46 MB (15.53%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    int solve(int i, int amount, vector<int>& nums, vector<vector<int>>& t){
-        if(amount == 0){
-            return 1;
-        }
-        if(i == 0){
-            if(amount % nums[0] == 0) return 1;
-            return 0;
-        }
-
-        if(t[i][amount] != -1){
-            return t[i][amount];
-        }
-
-        int notTake = solve(i-1, amount, nums, t);
-
-        int take = 0;
-        if(nums[i] <= amount){
-            take = solve(i, amount-nums[i], nums, t);
-        }
-        return t[i][amount] = take + notTake;
-    }
     int change(int amount, vector<int>& coins) {
-       int n = coins.size();
-       vector<vector<int>> t(n, vector<int>(amount+1, -1));
-       return solve(n-1, amount, coins, t); 
+        int n = coins.size();
+
+        vector<vector<uint64_t>> t(n, vector<uint64_t>(amount+1, 0));
+
+        for(int i=0; i<n; i++){
+            t[i][0] = 1;
+        }
+
+        for(int i=0; i<=amount; i++){
+            if(i % coins[0] == 0){
+                t[0][i] = 1;
+            }
+            else{
+                t[0][i] = 0;
+            }
+        }
+
+        for(int i=1; i<n; i++){
+            for(int j=1; j<amount+1; j++){
+                uint64_t notTake = t[i-1][j];
+                uint64_t take = 0;
+                if(coins[i] <= j){
+                    take = t[i][j-coins[i]];
+                }
+                t[i][j] = take + notTake;
+            }
+        }
+        return t[n-1][amount]; 
     }
 };
