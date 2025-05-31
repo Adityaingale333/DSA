db1df49b2f7293472c754a96782eb803a8a6e8fa
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-05-31 23:10:11 +0530

    Time: 28 ms (73.3%), Space: 31.9 MB (15.05%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool solve(int i, string& s, int j, string& p, vector<vector<int>>& t){
+   /* bool solve(int i, string& s, int j, string& p, vector<vector<int>>& t){
         if(i < 0 && j < 0){
             return true;
         }
@@ -26,13 +26,45 @@ public:
             return t[i][j] = solve(i-1, s, j, p, t) || solve(i, s, j-1, p, t);
         }   
         return false;            
-    }
+    }*/
     bool isMatch(string s, string p) {
         int m = s.size();
         int n = p.size();
 
-        vector<vector<int>> t(m, vector<int>(n, -1));
+        vector<vector<int>> t(m+1, vector<int>(n+1, 0));
 
-        return solve(m-1, s, n-1, p, t);
+        t[0][0] = 1; // both string matched and exhausted
+
+        for(int i=1; i<m+1; i++){
+            t[i][0] = 0; // p exhausted but not string
+        }
+
+        for(int j=1; j<n+1; j++){
+            if(p[j-1] == '*'){
+                t[0][j] = 1;
+            }
+            else{
+                for(int k=j; k<n+1; k++){
+                    t[0][k] = 0;
+                }
+                break;
+            }
+        }
+
+        for(int i=1; i<m+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(s[i-1] == p[j-1] || p[j-1] == '?'){
+                    t[i][j] = t[i-1][j-1];
+                }
+                else if(p[j-1] == '*'){
+                    t[i][j] = t[i-1][j] || t[i][j-1];
+                }
+                else{
+                    t[i][j] = 0;
+                }
+            }
+        }
+
+        return t[m][n] ;
     }
 };
