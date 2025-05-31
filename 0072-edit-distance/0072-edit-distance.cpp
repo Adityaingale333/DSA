3c789cec37259e110fea29a449f8605524aa4a95
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-05-31 19:31:13 +0530

    Time: 3 ms (91.01%), Space: 13 MB (74.67%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    // using lcs we can only compute insert and delete operations
+    // to compute all 3 insert delete and replace, we need to write recurssion of match - notMatch
+    int solve(int i, string&word1, int j, string& word2, vector<vector<int>>& t){
+        if(j < 0){
+            return i+1; // word2 in finished, so we will need to delete word1 of i+1 length
+        }
+        if(i < 0){
+            return j+1; // word1 is finished, so we will need to insert the remaining word2 of j+1 length
+        }
+
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
+        if(word1[i] == word2[j]){
+            return t[i][j] = solve(i-1, word1, j-1, word2, t);
+        }
+
+        // if above condition is false, then only this below will run
+        int insert = 1 + solve(i, word1, j-1, word2, t); // only i will move
+        int remove = 1 + solve(i-1, word1, j, word2, t); // only j will move
+        int replace = 1 + solve(i-1, word1, j-1, word2, t); // both will move
+
+        return t[i][j] = min(insert, min(remove, replace));
+    }
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+
+        vector<vector<int>> t(m, vector<int>(n, -1));
+        return solve(m-1, word1, n-1, word2, t);
+    }
+};
