9ef992c9b81fb2e2be4422631efe95fc599ae8ef
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-06-02 10:28:46 +0530

    Time: 0 ms (100%), Space: 13.2 MB (50.79%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int i, int j, int m, vector<vector<int>>& triangle, vector<vector<int>>& t){
+    /*int solve(int i, int j, int m, vector<vector<int>>& triangle, vector<vector<int>>& t){
         if(i==m-1){
             return triangle[i][j];
         }
@@ -17,11 +17,24 @@ public:
         int nextIdx = triangle[i][j] + solve(i+1, j+1, m, triangle, t);
 
         return t[i][j] = min(sameIdx, nextIdx);
-    }
+    }*/
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
         int n = triangle[m-1].size();
-        vector<vector<int>> t(m, vector<int>(n, -1));
-        return solve(0, 0, m, triangle, t);
+
+        vector<vector<int>> t(m, vector<int>(n, 0));
+
+        for(int j=0; j<n; j++){
+            t[m-1][j] = triangle[m-1][j];
+        }
+
+        for(int i=n-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                int sameIdx = triangle[i][j] + t[i+1][j];
+                int nextIdx = triangle[i][j] + t[i+1][j+1];
+                t[i][j] = min(sameIdx, nextIdx);
+            }
+        }
+        return t[0][0] ;
     }
 };
