19829ae16f41fc5325dae37f8afb1dc65c6c706b
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-06-03 19:55:37 +0530

    Time: 8 ms (10.05%), Space: 19.6 MB (27.55%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int solve(int i, int buy, vector<int>& prices, vector<vector<int>>& t){
-        if(i == prices.size()){
-            return 0;
-        }
-
-        if(t[i][buy] != -1){
-            return t[i][buy];
-        }
-
-        int profit = 0;
-
-        if(buy){
-            profit = max(-prices[i] + solve(i+1, 0, prices, t), 0 + solve(i+1, 1, prices, t));
-        }
-        else{
-            profit = max(prices[i] + solve(i+1, 1, prices, t), 0 + solve(i+1, 0, prices, t));
-        }
-        return t[i][buy] = profit;
-    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> t(n, vector<int>(2, -1));
-        return solve(0, 1, prices, t);
+
+        vector<vector<int>> t(n+1, vector<int>(2, 0)); // buy.size() == 2, 0 and 1, 0 means cannot buy
+       
+        t[n][0] = t[n][1] = 0;
+        for(int i=n-1; i>=0; i--){
+            for(int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if(buy){
+                    profit = max(-prices[i]+t[i+1][0], t[i+1][1]);
+                }
+                else{
+                    profit = max(prices[i]+t[i+1][1], t[i+1][0]);
+                }
+                t[i][buy] = profit;
+            }
+        }
+        return t[0][1] ;
     }
 };
