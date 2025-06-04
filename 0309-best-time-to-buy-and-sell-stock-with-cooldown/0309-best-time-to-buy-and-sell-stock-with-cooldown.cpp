daf84bd7f5b1d400a215ad10cd54be2bf7654cda
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-06-04 19:40:06 +0530

    Time: 3 ms (34.75%), Space: 14.9 MB (93.94%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -21,24 +21,31 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
 
-        vector<vector<int>> t(n+2, vector<int>(2, 0));
+        /*vector<vector<int>> t(n+2, vector<int>(2, 0));
 
         for(int buy=0; buy<2; buy++){
             t[n+1][buy] = t[n][buy] = 0;
-        }
+        }*/
+
+        // space optimization, using vector
+        vector<int> after2(2, 0);
+        vector<int> after1(2, 0); // we need two vector bcz we are dealing with n+2
+        vector<int> curr(2, 0);
 
         for(int i=n-1; i>=0; i--){
             for(int buy=0; buy<2; buy++){
                 int profit = 0;
                 if(buy){
-                    profit = max( -prices[i] + t[i+1][0], t[i+1][1] );
+                    profit = max( -prices[i] + after1[0], after1[1] );
                 }
                 else{
-                    profit = max( prices[i] + t[i+2][1], t[i+1][0] );
+                    profit = max( prices[i] + after2[1], after1[0] );
                 }
-                t[i][buy] = profit;
+                curr[buy] = profit;
             }
+            after2 = after1;
+            after1 = curr;
         }
-        return t[0][1];
+        return after1[1];
     }
 };
