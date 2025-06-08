21d20d79d1b3327e0d591e0b1c2dc13744053806
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-06-08 14:06:51 +0530

    Time: 792 ms (22.22%), Space: 345.4 MB (22.22%) - LeetHub

--- 3892-best-time-to-buy-and-sell-stock-v/3892-best-time-to-buy-and-sell-stock-v.cpp
+++ 3892-best-time-to-buy-and-sell-stock-v/3892-best-time-to-buy-and-sell-stock-v.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    long long solve(int i, int transaction, int state, int k, vector<int>& prices, vector<vector<vector<long long>>>& dp) {
+    /*long long solve(int i, int transaction, int state, int k, vector<int>& prices, vector<vector<vector<long long>>>& dp) {
         int n = prices.size();
         if (i >= n ){
             if(state != 0) return -1e9; // index khtm ho gaye aur state 0 nahi hai, matlb koi to transaction baki hai, isliye -infinity
@@ -38,7 +38,7 @@ public:
         }
 
         return dp[i][transaction][state] = profit;
-    }
+    }*/
 
     long long maximumProfit(vector<int>& prices, int k) {
         int n = prices.size();
@@ -46,8 +46,46 @@ public:
         // 0. -> do anything
         // 1. -> can only sell or hold 
         // 2. -> can only buyback or hold
-        vector<vector<vector<long long>>> dp(n+1, vector<vector<long long>>(k+1, vector<long long>(3, -1)));
-        return solve(0, 0, 0, k, prices, dp);
+        vector<vector<vector<long long>>> t(n+1, vector<vector<long long>>(k+1, vector<long long>(3, 0)));
+
+        // jo base case 0 nahi hai, wahi likhunga, kyuki baki 0 se hi initialize kra hai maine
+        for(int transaction=0; transaction<k+1; transaction++){
+            for(int state = 1; state<=2; state++){
+                t[n][transaction][state] = -1e9;
+            }
+        }
+
+        for(int i=n-1; i>=0; i--){ // i == n pe profit 0 bcz out of bound index
+            for(int transaction=k-1; transaction>=0; transaction--){ // transaction == k pe 0 bcz transaction khtm ho gaye
+                for(int state=2; state>=0; state--){
+                    long long profit = 0;
+
+                    if (state == 0) {
+                        // kuch bhi kr skte hai, buy sell skip
+                        long long skip = t[i+1][transaction][0];
+                        long long buy = -prices[i] + t[i+1][transaction][1];
+                        long long shortSell = prices[i] + t[i+1][transaction][2];
+                        profit = max({skip, buy, shortSell});
+                    }
+                    else if (state == 1) {
+                        // buy kr liya hai ab -> ya to sell or hold
+                        long long sell = prices[i] + t[i+1][transaction+1][0]; 
+                        long long hold = t[i+1][transaction][1];
+                        profit = max(sell, hold);
+                    }
+                    else {
+                        // sell kr dia hai ab -> ya to buy back or hold
+                        long long buyBack = -prices[i] + t[i+1][transaction+1][0]; 
+                        long long hold = t[i+1][transaction][2];
+                        profit = max(buyBack, hold);
+                    }
+
+                    t[i][transaction][state] = profit;
+                }
+            }
+        }
+        
+        return t[0][0][0] ;
     }
 };
 
