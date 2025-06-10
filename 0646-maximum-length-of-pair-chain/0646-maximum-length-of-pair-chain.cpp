39e4c5aaefc22b5557f8a10788cb733a325e7a1e
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-06-10 12:19:45 +0530

    Time: 170 ms (26.9%), Space: 163.6 MB (5.7%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int solve(int i, int l, int r, int idx, vector<vector<int>>& pairs, vector<vector<int>>& t){
-        if(i < 0){
-            return 0;
-        }
-
-        if(t[i][idx] != -1){
-            return t[i][idx];
-        }
-
-        int notTake = solve(i-1, l, r, idx, pairs, t);
-
-        int take = 0;
-        if(idx == pairs.size() || pairs[i][r] < pairs[idx][l]){
-            take = 1 + solve(i-1, l, r, i, pairs, t);
-        }
-        return t[i][idx] = max(take, notTake);
-    }
+   
     int findLongestChain(vector<vector<int>>& pairs) {
         int n = pairs.size();
         sort(pairs.begin(), pairs.end());
-        vector<vector<int>> t(n, vector<int>(n+1, -1));
-        return solve(n-1, 0, 1, n, pairs, t); // n == idx, to start comparing
+        vector<vector<int>> t(n+1, vector<int>(n+1, 0));
+
+        for(int i=n-1; i>=0; i--){
+            for(int lastIdx=i-1; lastIdx>=-1; lastIdx--){
+                int notTake = t[i+1][lastIdx+1];
+                int take = 0;
+                if(lastIdx == -1 || pairs[i][0] > pairs[lastIdx][1]){
+                    take = 1 + t[i+1][i+1];
+                }
+                t[i][lastIdx+1] = max(take, notTake);
+            }
+        }
+        return t[0][-1+1] ;
     }
 };
