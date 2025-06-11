e7825c6501b2ebcbfe118cdab81abcb19cf38da4
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-06-12 00:20:30 +0530

    Time: 64 ms (65.08%), Space: 75.9 MB (43.8%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int solve(int i, int j, string& s, string& s2, vector<vector<int>>& t){
-        if(i == s.size() || j == s2.size()){
-            return 0;
-        }
-
-        if(t[i][j] != -1){
-            return t[i][j];
-        }
-
-        if(s[i] == s2[j]){
-            return t[i][j] = 1 + solve(i+1, j+1, s, s2, t);
-        }
-        else{
-            return t[i][j] = max(solve(i+1, j, s, s2, t), solve(i, j+1, s, s2, t));
-        }
-
-    }
     int longestPalindromeSubseq(string s) {
         int n = s.size();
-        string s2 = s;
-        reverse(s2.begin(), s2.end());
 
-        vector<vector<int>> t(n, vector<int>(n, -1));
-        return solve(0, 0, s, s2, t);
+        // t[i][j] -> states, longest pallindrome subsequence from index i to j 
+        vector<vector<int>> t(n, vector<int>(n, 0));
+
+        // if length 1, then it will always be a pallindrome, it will help in filling dp table
+        for(int i=0; i<n; i++){
+            t[i][i] = 1;
+        }
+
+        for(int len=2; len<=n; len++){
+            for(int i=0; (i + len-1)<n; i++){
+                int j = i + len-1;
+
+                if(s[i] == s[j]){
+                    t[i][j] = 2 + t[i+1][j-1]; // 2 bcz i and j ch matches, and then we look for between them, t[i+1][j-1]
+                }
+                else{
+                    t[i][j] = max(t[i+1][j], t[i][j-1]);
+                }
+            }
+        }
+        return t[0][n-1]; // longest pallindromic subsequence from i to j
     }
 };
