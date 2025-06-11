b9088e88b0360256e7b9c6bdf8171a632b562885
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-06-11 23:31:13 +0530

    Time: 323 ms (18.19%), Space: 26.1 MB (34.34%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -4,28 +4,36 @@ public:
         int n = s.size();
         string ans = "";
 
-        int startIdx = -1;
-        int maxLen = 0;
+        // will use these two variable for generating s.substr()
+        int startIdx = -1; // variable for storing starting index
+        int maxLen = 0; // variable for storing maxlen
 
-        vector<vector<bool>> t(n, vector<bool>(n, false));
+        // 2D dp table for storing whether the substring from i to j, is pallindrome or not
+        vector<vector<bool>> t(n, vector<bool>(n, false)); 
 
-        for(int len=1; len<=n; len++){
-            for(int i=0; i + len-1<n; i++){
+        for(int len=1; len<=n; len++){ // we want substring of all possible lengths
+            for(int i=0; (i + len-1)<n; i++){ // these are the starting indexes of the substring off all lengths
+
+            // this is end index of the substring, we calculate it by the logic of length required, 
+            // we want len 4, so if i = 0, then j = 0 + (len-1), = 3
                 int j = i + len-1; 
 
-                if(i == j){
+                if(i == j){ // this is the case of substring of length 1, always pallindrome
                     t[i][j] = true;
                 }
-                else if(i+1 == j){
+                else if(i+1 == j){ // this is the case of substring of length 2, will be pallindrome if both s[i] and s[j] are same
                     t[i][j] = (s[i] == s[j]);
                 }
-                else{
+                // this is the general case, length > 2
+                // here we need tho check if the first and last character are same, and then we check if the
+                // remaining substring from i+1 to j-1, is palliindrome or not
+                else{ 
                     t[i][j] = (s[i] == s[j] && t[i+1][j-1]);
                 }
 
-                if(t[i][j] && j-i+1 > maxLen){
-                    startIdx = i;
-                    maxLen = j-i+1;
+                if(t[i][j] && j-i+1 > maxLen){ // if t[i][j] is pallindrome and we got new maxLen
+                    startIdx = i;              // then we update starting index 
+                    maxLen = j-i+1;            // and maxLen
                 }
 
             }
