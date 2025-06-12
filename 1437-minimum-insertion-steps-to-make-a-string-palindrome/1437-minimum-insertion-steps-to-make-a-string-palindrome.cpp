d6fed259178df64fb6f3afab4b0635b36b30f6fc
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-06-12 09:41:17 +0530

    Time: 31 ms (65.37%), Space: 31.3 MB (74.55%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    int lcs(string s, string s2){
-        int m = s.size();
-        int n = s2.size();
-
-        vector<vector<int>> t(m+1, vector<int>(n+1, 0));
-        for(int i=0; i<m+1; i++){
-            t[i][0] = 0;
-        }
-        for(int j=0; j<n+1; j++){
-            t[0][j] = 0;
+// recursion nad memoization
+    int solve(int i, int j, string& s, vector<vector<int>>& t){
+        if(i>j){
+            return 0;
         }
 
-        for(int i=1; i<m+1; i++){
-            for(int j=1; j<n+1; j++){
-                if(s[i-1] == s2[j-1]){
-                    t[i][j] = 1 + t[i-1][j-1];
-                }
-                else{
-                    t[i][j] = max(t[i-1][j], t[i][j-1]);
-                }
-            }
+        if(t[i][j] != -1){
+            return t[i][j];
         }
-        return t[m][n];
+
+        if(s[i] == s[j]){
+            return t[i][j] = solve(i+1, j-1, s, t);
+        }
+
+        return t[i][j] = min(1 + solve(i+1, j, s, t), 1 + solve(i, j-1, s, t));
     }
     int minInsertions(string s) {
-        string s2 = s;
-        reverse(s2.begin(), s2.end());
-
-        return s.size() - lcs(s, s2);
+        int n = s.size();
+        vector<vector<int>> t(n, vector<int>(n, -1));
+        return solve(0, n-1, s, t);
     }
 };
