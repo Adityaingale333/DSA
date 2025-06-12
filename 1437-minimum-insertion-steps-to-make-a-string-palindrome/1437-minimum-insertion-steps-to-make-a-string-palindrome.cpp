c4de35fc89f7867ee5c9b113b862e8413636410b
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-06-12 09:56:48 +0530

    Time: 23 ms (78.85%), Space: 31.4 MB (58.89%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
-// recursion nad memoization
-    int solve(int i, int j, string& s, vector<vector<int>>& t){
+    // *** Using Blueprint ***
+
+    // recursion nad memoization
+    /*int solve(int i, int j, string& s, vector<vector<int>>& t){
         if(i>j){
             return 0;
         }
@@ -15,10 +17,29 @@ public:
         }
 
         return t[i][j] = min(1 + solve(i+1, j, s, t), 1 + solve(i, j-1, s, t));
-    }
+    }*/
     int minInsertions(string s) {
         int n = s.size();
-        vector<vector<int>> t(n, vector<int>(n, -1));
-        return solve(0, n-1, s, t);
+
+        // t[i][j] -> states that minimum insertion required to make string i to j pallindrome 
+        vector<vector<int>> t(n, vector<int>(n, 0));
+
+        for(int i=0; i<n; i++){
+            t[i][i] = 0; // 0 bcz, single characters are always pallindrome, we do not need any insertions
+        }
+
+        for(int len=2; len<=n; len++){
+            for(int i=0; i + (len-1)<n; i++){
+                int j = i + (len-1); // j calculation is simpe, we konw len = j-i+1, then j = i+len-1
+
+                if(s[i] == s[j]){
+                    t[i][j] = t[i+1][j-1]; // if bot characters are equal, then we do not need any insertion
+                }
+                else{ // if characters do not match, then we need to insert that charaacter and star comparing next
+                    t[i][j] = min(1 + t[i+1][j], 1 + t[i][j-1]);
+                }
+            }
+        }
+        return t[0][n-1];
     }
 };
