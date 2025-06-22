af5115d714a849507784d8841b4a2dd9a503b51f
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-06-22 09:18:56 +0530

    Time: 0 ms (100%), Space: 9.7 MB (70.3%) - LeetHub

--- 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.cpp
+++ 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.cpp
@@ -4,24 +4,14 @@ public:
         int n = s.size();
         vector<string> ans;
 
-        if(n % k == 0){
-            for(int i=0; i<n; i+=k){
-                ans.push_back(s.substr(i, k));
+        for(int i=0; i<n; i+=k){
+            string temp = s.substr(i, k); // even if we do not have k char for the last substr, it will return
+                                        // substr with remainiing characters upto end of the string 
+            if(temp.length() < k){
+                temp.append(k-temp.length(), fill);// k-temp.length = 3-1 -> 2 fill char needed
             }
-            return ans;
+            ans.push_back(temp);
         }
-
-        int remChar = n % k;
-        for(int i=0; i<n-remChar; i+=k){ // jitne possible hai utne daal diye
-            ans.push_back(s.substr(i, k));
-        }
-
-        string last = s.substr(n-remChar);
-        while(last.length() != k){
-            last = last + fill;
-        }
-
-        ans.push_back(last);
         return ans;
     }
 };
