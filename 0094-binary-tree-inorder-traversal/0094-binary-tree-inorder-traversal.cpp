33399d372df4445ccb795adc9969d5922eed4a1f
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-06-23 00:35:13 +0530

    Time: 0 ms (100%), Space: 10.8 MB (87.58%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,22 +11,36 @@
  */
 class Solution {
 public:
-    // LEFT ROOT RIGHT
-    void inorder(TreeNode* root, vector<int>& ans){
-        // if root is null, just return
-        if(root == NULL){
-            return;
-        }
-
-        inorder(root->left, ans);
-
-        ans.push_back(root->val);
-
-        inorder(root->right, ans);
-    }
+    // MORRIS INORDER TRAVERSAL 
+    // we will make temporary threads from "root ke left subtree ka rightmost node se root tak"
+    // jisse hum bina backtracking use kre vapas root node pr jaa ske 
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        inorder(root, ans);
+        
+        TreeNode* curr = root;
+        while(curr != NULL){
+            if(curr->left == NULL){ // agar left hai hi nahi to chup chap root print krkr right chale jao
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{ // agar left hai toh......
+                TreeNode* prev = curr->left; // ek prev node lo usko left subtree ke rightmost node tak bhejo
+                while(prev->right != NULL && prev->right != curr){
+                    prev = prev->right;
+                }
+
+                if(prev->right == NULL){ // agar wo rightmost null ko point kr raha hai mtlb pehli baar aaye hai
+                    prev->right = curr;// isliye usko root ki taraf point krdo
+                    curr = curr->left;// ab hamare pass rightmost aur root ka connection hai toh ab aur left traverse karo
+                }
+
+                else{ // agar vo pehle se root ko point kr rha hai mtlb hum pehle bhi aa chuke hai
+                    prev->right = NULL; // isliye usko null point krke connection break kro
+                    ans.push_back(curr->val); // hume pura left traverse kr liya mtlb ab ye root hai, to apna ans me le liya
+                    curr = curr->right; // ab right traverse kro, mtlb fir se left subtree ka rightmost.............
+                }
+            }
+        }
         return ans;
     }
 };
