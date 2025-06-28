80c3fdbec91de3c08d7530112e9438460f3a1aa0
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-06-28 22:52:21 +0530

    Time: 35 ms (39.38%), Space: 17.4 MB (89.38%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -3,32 +3,33 @@ public:
     int findNumberOfLIS(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> dp(n, 1); // dp[i] stores the length of the LIS ending at index i
-        vector<int> count(n, 1); // count[i] stores the number of LIS of maximum length ending at index i
-        int len = 0; // Maximum length of the LIS
+        vector<int> t(n, 1); // LIS ending at i
+        vector<int> count(n, 1); // count of LIS ending at i
 
         for(int i=0; i<n; i++){
             for(int j=0; j<i; j++){
-                // If we found a longer subsequence ending at i
-                if(nums[i] > nums[j] && dp[j]+1 > dp[i]){
-                    dp[i] = dp[j] + 1;
-                    count[i] = count[j]; // Reset the count to the number of ways to form subsequences ending at j
-                }
-                else if(nums[i] > nums[j] && dp[j]+1 == dp[i]){ // If we found another subsequence of the same length ending at i
-                    
-                    // Add the number of ways to form subsequences ending at j
-                    count[i] = count[i] + count[j];
-                }
-            }   
-            len = max(len, dp[i]);
-        }
+                if(nums[j] < nums[i]){
 
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            if(dp[i] == len){
-                ans = ans + count[i]; // Sum up the number of LIS with the maximum length
+                    if(t[j] + 1 == t[i]){
+                        count[i] = count[i] + count[j];
+                    }
+                    else if(t[j] + 1 > t[i]){
+                        t[i] = t[j] + 1;
+                        count[i] = count[j];
+                    }
+
+                }
             }
         }
-        return ans;
+
+        int maxVal = *max_element(t.begin(), t.end());
+        int ansCount = 0;
+
+        for(int i=0; i<n; i++){
+            if(t[i] == maxVal){
+                ansCount = ansCount + count[i];
+            }
+        }
+        return ansCount;
     }
 };
