cdefdf4d7da057d1fed555e52b062cbfd343d7cb
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-06-30 00:46:26 +0530

    Time: 0 ms (100%), Space: 19.2 MB (7.05%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     // bipartite means, can we divide the graph into two colours, such that no adjacent nodes are of same colour
-    bool dfs(int curr, vector<vector<int>>& graph, vector<int>& color, int currColor){
+    bool bfs(int curr, vector<vector<int>>& graph, vector<int>& color, int currColor){
         color[curr] = currColor;
+        queue<int> q;
+        q.push(curr);
 
-        for(auto& v : graph[curr]){
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
 
-            if(color[curr] == color[v]){ // if the neighbour is already coloured, and it is same as curr, and it is not bipartite
-                return false;
+            for(auto& v : graph[u]){
+                if(color[curr] == color[v]){ // if the neighbour is already coloured, and it is same as curr, and it is not bipartite
+                    return false;
+                }
+
+                else if(color[v] == -1){ // means this neighbour is not colored
+                    int colorOfV = 1 - currColor; // if already 0 then -> 1-0 = 1, else if already 1 then -> 1-1 = 0 
+                    bool check = bfs(v, graph, color, colorOfV);
+                    if(check == false) return false; // written in simiplified form
+                }
             }
-
-            else if(color[v] == -1){ // means this neighbour is not colored
-                int colorOfV = 1 - currColor; // if already 0 then -> 1-0 = 1, else if already 1 then -> 1-1 = 0 
-                bool check = dfs(v, graph, color, colorOfV);
-                if(check == false) return false; // written in simiplified form
-            }
-
         }
 
         return true;
@@ -29,7 +34,7 @@ public:
         for(int i=0; i<n; i++){
             if(color[i] == -1){
 
-                if(dfs(i, graph, color, 0) == false){ // 0 -> means i am colouring red 
+                if(bfs(i, graph, color, 0) == false){ // 0 -> means i am colouring red 
                     return false;
                 } 
 
