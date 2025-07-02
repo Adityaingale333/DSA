6f7982df887c89f9ea1f8324f94dfe4c926e3f4b
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-07-02 23:57:42 +0530

    Time: 0 ms (100%), Space: 71.4 MB (69.18%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,46 +1,56 @@
 class Solution {
 public:
-    int subarrayK(vector<int>&nums, int k){      // all subarray <= k
+    /*Let's see this with an example : 
+    nums = {2, 1}, k = 1
+    we have two subarrays having oddCount --> {2, 1}, {1}
+    COUNT_NICE_SUBARRAY = 2
 
-        if(k<0)return 0;   // edge case, if ask for 0, then return 0, bcz (0-1) = -1 
+    Now, suppose we have one more integer 2 . our array looks like --> {2, 1, 2}, k = 1
+    If you see, addition of this extra 2 will not impact the oddCount because this 2 is even.
+    So, we again get two more subarrays by including this 2 with previous two nice subarrays {2, 1} and {1}
+    so we get {2, 1, 2}, {1, 2}
+    So, over all Nice subarrays becomes - {2, 1}, {1}, {2, 1, 2}, {1, 2}
+    Now, COUNT_NICE_SUBARRAY = 4
 
-        int l=0, r=0, sum=0, count=0;
 
-        while(r<nums.size()){
-            sum += nums[r]%2;       // add up all elments till less than k
-                                    // mod by 2, so that if even add 0, or else 1
+    Now, let's take one more example : Let's say the one more integer after {2, 1} is 1
+    So, our array becomes -> {2, 1, 1}
+    Let's break it again -> from {2, 1} we had {2, 1}, {1} i.e. COUNT_NICE_SUBARRAY = 2
 
-            while(sum>k){         // if sum exceeds k then subtract element from l
-                sum -= nums[l]%2;   // mod by 2, so that if even subtract 0 or else 1  
-                l++;
-            }
-
-            count += (r-l+1);   // add count of all possible subarray
-            r++;
-        }
-
-        return count;
-    }
+    Now, when 1 comes, this is another Odd number so oddCount is affacted and hence we can't add previousCount = 2 to our result because {2, 1, 1} doesn't have k = 1 odd numbers. It has 2 odd numbers.
+    So, we need to start from fresh and hence prevCount is made 0.
+    Now, the new subarray having k = 1 oddCount is the {1} 
+    so we got one nice subarray now.
+    COUNT_NICE_SUBARRAY = 2 + 1 = 3 */
 
     int numberOfSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
 
-        // improved the time complexity by removing this loop and using %2 in 
-        // function itself while adding or subtracting
+        int oddCount = 0;
+        int prevCount = 0;
+        int ans = 0;
 
-        /*for(int i=0; i<n; i++){   // convert everything to 1, 0
-            if(nums[i]%2 == 0){   // after that this question is similar to binary sum  
-                nums[i] = 0;      // subarray question
-            }
-            else{
-                nums[i] = 1;
-            }
-        }*/
+        int l=0, r=0;
 
-        int subarrayk = subarrayK(nums, k);  // all subarray less than equal to k
-        int subarraykLessOne = subarrayK(nums, k-1);//all subarray less than equal to k-1
-        int ans = subarrayk - subarraykLessOne ;
-        
+        while(r < n){
+            if(nums[r] % 2 == 1){
+                oddCount++;
+                prevCount = 0;
+            }
+
+            while(oddCount == k){
+                prevCount++;
+
+                if(r < n && nums[l] % 2 == 1){
+                    oddCount--;
+                }
+                l++;
+            }
+
+            ans = ans + prevCount;
+            r++;
+        }
+
         return ans;
     }
 };
