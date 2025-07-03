6da7b07fbf8f061b635bbdd618d21d893a8220c7
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-07-04 00:21:55 +0530

    Time: 0 ms (100%), Space: 46.3 MB (13.33%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
+    // we need to pick consecutively either from front or back, so we can not just take sum of k largest element
+    // what we can do is generate all combinations, sum of front k, then sum of front k-1 + last from back, 
+    // then sum of front k-2 +  2 from back, ............ 
     int maxScore(vector<int>& cardPoints, int k) {
         int n = cardPoints.size();
-        int lsum = 0;
-        int r=n-1;
+        int leftSum = 0;
 
         for(int i=0; i<k; i++){
-            lsum = lsum + cardPoints[i]; 
+            leftSum = leftSum + cardPoints[i];
         }
 
-        int maxsum = lsum;
-        int rsum = 0;
+        int r = n-1; // for rightsum, iterate from back
+        int rightSum = 0;
+        int maxSum = leftSum;
 
         for(int i=k-1; i>=0; i--){
-            lsum = lsum - cardPoints[i];
-            rsum = rsum + cardPoints[r];
+            leftSum = leftSum - cardPoints[i];
+            rightSum = rightSum + cardPoints[r];
             r--;
-            maxsum = max(maxsum, lsum+rsum);
+
+            maxSum = max(maxSum, leftSum + rightSum);
         }
-        return maxsum;
+
+        return maxSum;
     }
 };
