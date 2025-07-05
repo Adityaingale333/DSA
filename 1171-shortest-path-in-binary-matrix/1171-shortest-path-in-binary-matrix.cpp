aef89c35aad6750afb89af1fffb55a34caa24912
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-07-05 12:07:18 +0530

    Time: 13 ms (83.09%), Space: 23.5 MB (92.83%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
-    // we can also say we are using simple bfs, with distance thing
-    //                              OR
-    // while using dojkstra we will not rquire PQ here, bcz we have unit weights, so we will already get the dist
-    // in sorted order such as 1 1 1 1......  ,  2 2 2 2....  ,  3 3 3 3........ 
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
 
-        if(n == 1) return grid[0][0] == 0 ? 1 : -1;
-
-        vector<vector<int>> dist(n, vector<int>(n, 1e9));
-        dist[0][0] = 1;
-
-        queue<pair<int, pair<int,int>>> q; // (distance, {row, col});
-        q.push({1, {0,0}}); // dist, {row, col}
+        int level = 0;
+        queue<pair<int,int>> q; // row, col
+        q.push({0,0});
 
         int drow[] = {-1, -1, 0, 1, 1, 1, 0, -1};
         int dcol[] = {0, 1, 1, 1, 0, -1, -1, -1};
-
         while(!q.empty()){
-            int d = q.front().first;
-            int row = q.front().second.first;
-            int col = q.front().second.second; 
-            q.pop();
+            int k = q.size();
 
-            for(int i=0; i<8; i++){
-                int nrow = row + drow[i];
-                int ncol = col + dcol[i];
+            for(int i=0; i<k; i++){
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
 
-                if(nrow < n && nrow >= 0 && ncol < n && ncol >= 0 && grid[nrow][ncol] == 0 && (d + 1 < dist[nrow][ncol]) ){
-                    dist[nrow][ncol] = d + 1;
+                if(row == n-1 && col == n-1) return level + 1;
 
-                    if(nrow == n-1 && ncol == n-1) return d + 1;
+                for(int i=0; i<8; i++){
+                    int nrow = row + drow[i];
+                    int ncol = col + dcol[i];
 
-                    q.push({dist[nrow][ncol], {nrow, ncol}});
+                    if(nrow < n && nrow >= 0 && ncol < n && ncol >= 0 && grid[nrow][ncol] == 0){
+                        grid[nrow][ncol] = 1;
+                        q.push({nrow, ncol});
+                    }
                 }
             }
+            level++;
         }
 
         return -1;
