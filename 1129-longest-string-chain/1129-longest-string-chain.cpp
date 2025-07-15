16dcb663a1cdd96a3b94ee110332710f65bd0f14
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-07-15 17:09:04 +0530

    Time: 37 ms (82.31%), Space: 18.6 MB (84.8%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -3,51 +3,59 @@ public:
     static bool myFunction(string& word1, string& word2){
         return word1.length() < word2.length();
     }
-
-    bool isPredecessor(string& prevString, string& currString){
-        int n = prevString.length();
-        int m = currString.length();
+    bool isPredecessor(string& prev, string& curr){
+        int n = prev.length();
+        int m = curr.length();
 
         if(n > m || m-n != 1) return false;
-        
+
         int i=0, j=0;
 
         while(i<n && j<m){
-            if(prevString[i] == currString[j]){
-                i++, j++;
+            if(prev[i] == curr[j]){
+                i++;
+                j++;
             }
             else{
                 j++;
             }
         }
-        return (i == n) ;
+        return (i == n );
     }
-    int solve(int i, int prev, vector<string>& words,  vector<vector<int>>& t){
+    /*int solve(int i, int prevIdx, vector<string>& words){
         if(i == words.size()){
             return 0;
         }
 
-        if(t[i][prev + 1] != -1){
-            return t[i][prev + 1];
-        }
-
         int take = 0;
-        if(prev == -1 || isPredecessor(words[prev], words[i])){
-            take = 1 + solve(i+1, i, words, t);
+        if(prevIdx == -1 || isPredecessor(words[prevIdx], words[i])){
+            take = 1 + solve(i+1, i, words);
         }
 
-        int notTake = solve(i+1, prev, words, t);
+        int notTake = solve(i+1, prevIdx, words);
 
-        return t[i][prev + 1] = max(take, notTake);
-    }
-
+        return max(take, notTake);
+    }*/
     int longestStrChain(vector<string>& words) {
         int n = words.size();
 
         sort(words.begin(), words.end(), myFunction);
 
-        vector<vector<int>> t(n, vector<int>(n+1, -1));
+        vector<int> t(n, 1);
 
-        return solve(0, -1, words, t);
+        int maxLen = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(isPredecessor(words[j], words[i])){
+                    if(t[j] + 1 > t[i]){
+                        t[i] = t[j] + 1;
+                    }
+                }
+            }
+            maxLen = max(maxLen, t[i]);
+        }
+
+        return maxLen;
     }
 };
