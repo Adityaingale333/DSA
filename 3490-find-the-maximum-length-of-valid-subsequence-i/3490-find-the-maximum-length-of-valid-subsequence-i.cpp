f55d766b08fdaf042e8c1cd344d08b832a72503d
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-07-16 11:29:14 +0530

    Time: 147 ms (12.06%), Space: 144.4 MB (11.18%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int i, int state, vector<int>& nums, vector<vector<int>>& t){
+    /*int solve(int i, int state, vector<int>& nums, vector<vector<int>>& t){
         if(i == nums.size()){
             return 0;
         }
@@ -24,19 +24,45 @@ public:
         int notTake = solve(i+1, state, nums, t);
 
         return t[i][state + 1] = max(take, notTake);
-    }
+    }*/
 
     int maximumLength(vector<int>& nums) {
         int n = nums.size();
-
+        
+        // for recursion
         // state = -1, 0, 1
         // -1 -> kuch bhi utha le
         // 0 -> sirf even utha skta hai
         // 1 -> sirf odd utha skta hai
 
-        vector<vector<int>> t(n, vector<int>(3, -1));
+        vector<vector<int>> t(n+1, vector<int>(3, 0));
 
-        int oddEven = solve(0, -1, nums, t);
+        // for bottom up
+        // state = 0, 1, 2
+        // 0 -> kuch bhi utha le
+        // 1 -> sirf even utha skta hai
+        // 2 -> sirf odd utha skta hai
+        for(int i=n-1; i>=0; i--){
+            for(int state=0; state<3; state++){
+                int take = 0;
+                if(state == 0){
+                    int nextState = (nums[i] % 2 == 0) ? 2 : 1;
+                    take = 1 + t[i+1][nextState];
+                }
+                else if(state == 1 && nums[i] % 2 == 0){
+                    take = 1 + t[i+1][2];
+                }
+                else if(state == 2 && nums[i] % 2 == 1){
+                    take = 1 + t[i+1][1];
+                }
+                
+                int notTake = t[i+1][state];
+
+                t[i][state] = max(take, notTake);
+            }
+        }
+
+        int oddEven = t[0][0];
 
         int even = 0;
         int odd = 0;
