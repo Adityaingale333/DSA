ffb0d13d8593b7cd96cd535e9a850ddb2182b136
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-07-19 00:22:44 +0530

    Time: 0 ms (100%), Space: 19.7 MB (36.12%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -11,36 +11,33 @@
  */
 class Solution {
 public:
-/* The width of one level is defined as the length between the end-nodes (the leftmost and rightmost non-null nodes), 
-where the null nodes between the end-nodes that would be present in a complete binary tree 
-extending down to that level are also counted into the length calculation.*/
-
-// this above para simply means that we have to assume the tree as a complete binary tree
-// the only imp point to notice is we have to take care of end nodes, otherwise we could have directly return 2^n
-// we can CBT indexing as, root(0) -> left(1), right(2) ==> i=0 -> 2*i+1 = 1, 2*1+2 = 2
-
     int widthOfBinaryTree(TreeNode* root) {
-        queue<pair<TreeNode*, unsigned long long>> q; // node -> index
+        if(root == NULL) return 0;
+
+        queue<pair<TreeNode*, unsigned long long>> q;
         q.push({root, 0});
 
-        // to get width of any level we will need starting and end index of that level
-        int maxWidth = 0;
+        unsigned long long maxWidth = 0;
+
         while(!q.empty()){
-            unsigned long long startIdx = q.front().second; // <first node of a level, first index>
-            unsigned long long endIdx = q.back().second; // <second node of a level, last index>
-            maxWidth = max(maxWidth, (int)(endIdx - startIdx + 1));
+            unsigned long long start = q.front().second;
+            unsigned long long end = q.back().second;
+
+            maxWidth = max(maxWidth, end - start + 1);
 
             int n = q.size();
 
-            for(int i=0; i<n; i++){ // we need first and last node of a level to find length, so we club all nodes of level together
-                TreeNode* node = q.front().first; // node of level
-                unsigned long long j = q.front().second; // index of that node, which we will use to give index to its child
-                q.pop(); // karde bhai, nahi toh memory limit exceed de raha hai
+            for(int i=0; i<n; i++){
+                TreeNode* node = q.front().first;
+                unsigned long long idx = q.front().second;
+                q.pop();
 
-                if(node->left != NULL) q.push({node->left, 2*j+1});// even if it is null then that index will be empty
-                if(node->right != NULL) q.push({node->right, 2*j+2});
+                if(node->left != NULL) q.push({node->left, (2*idx)+1});
+
+                if(node->right != NULL) q.push({node->right, (2*idx)+2});
             }
         }
-        return maxWidth;
+
+        return (int)maxWidth;
     }
 };
