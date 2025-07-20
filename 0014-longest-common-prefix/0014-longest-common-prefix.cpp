e8414547f8c27700af59a33b625012209f43ebd3
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-07-20 20:15:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -2,21 +2,29 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         int n = strs.size();
-        string prefix = strs[0];
 
-        for(int i=1; i<n; i++){
-            int j=0;
+        sort(strs.begin(), strs.end(), [](string& s1, string&s2){
+            return s1.length() < s2.length();
+        });
 
-            // j < prefix and j < word we are comparing with and jab tak letters equal hai
-            while(j < prefix.size() && j < strs[i].size() && prefix[j] == strs[i][j]){
-                j++;
+        string s1 = strs[0];
+        int minIdx = INT_MAX;
+
+        for(int k=1; k<n; k++){
+            string s2 = strs[k];
+            int i=0, j=0;
+
+            while(i<s1.size() && j<s2.size()){
+                if(s1[i] != s2[j]){
+                    if(i==0) i = INT_MAX;
+                    break;
+                }
+                i++, j++;
             }
 
-            prefix = prefix.substr(0,j);
-
-            if(prefix.empty()) break;
+            minIdx = min(minIdx, i);
         }
 
-        return prefix;
+        return minIdx == INT_MAX ? "" : s1.substr(0, minIdx);
     }
 };
