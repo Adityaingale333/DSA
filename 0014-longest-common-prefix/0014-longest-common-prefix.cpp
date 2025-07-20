781478e69d626e21c06608e3c1198dd3041ff8a4
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-07-20 20:20:02 +0530

    Time: 0 ms (100%), Space: 12.1 MB (33.39%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,12 +3,7 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         int n = strs.size();
 
-        sort(strs.begin(), strs.end(), [](string& s1, string&s2){
-            return s1.length() < s2.length();
-        });
-
         string s1 = strs[0];
-        int minIdx = INT_MAX;
 
         for(int k=1; k<n; k++){
             string s2 = strs[k];
@@ -16,15 +11,16 @@ public:
 
             while(i<s1.size() && j<s2.size()){
                 if(s1[i] != s2[j]){
-                    if(i==0) i = INT_MAX;
                     break;
                 }
                 i++, j++;
             }
 
-            minIdx = min(minIdx, i);
+            s1 = s1.substr(0, i);
+
+            if(s1 == "") return "";
         }
 
-        return minIdx == INT_MAX ? "" : s1.substr(0, minIdx);
+        return s1;
     }
 };
