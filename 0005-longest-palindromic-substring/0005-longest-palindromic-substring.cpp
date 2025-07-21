0d0eeec5ea046b2e5211269d060e860d44b79e7b
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-07-21 23:05:21 +0530

    Time: 4 ms (94.17%), Space: 9.4 MB (82.13%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    void solve(int l, int r, string& s, int& startIdx, int& maxLen){
+    // expand from centre, keeping track of longest pallindrome
+    void solve(int l, int r, string& s, int& maxLen, int& startIdx){
         while(l>=0 && r<s.length() && s[l] == s[r]){
             l--;
             r++;
         }
 
+        // after coming out of the loop, l and r will be off by one, so take care of that
         if( (r-1) - (l+1) + 1 > maxLen){
-            startIdx = l+1;
             maxLen = (r-1) - (l+1) + 1;
+            startIdx = l+1 ;
         }
     }
-    string longestPalindrome(string s){
-        int n = s.size();
-        int startIdx = -1;
+    string longestPalindrome(string s) {
+        int n = s.length();
+
         int maxLen = 0;
+        int startIdx = 0;
 
         for(int i=0; i<n; i++){
-            solve(i, i, s, startIdx, maxLen);
+            solve(i, i, s, maxLen, startIdx); // considering odd length pallindrome
 
-            solve(i, i+1, s, startIdx, maxLen);
+            solve(i, i+1, s, maxLen, startIdx); // considering even length pallindrome
         }
+
         return s.substr(startIdx, maxLen);
     }
 };
