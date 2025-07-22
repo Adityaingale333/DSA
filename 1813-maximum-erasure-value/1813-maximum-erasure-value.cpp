d6c5d239557853cdb94e24d9fcf954722e5c6dd6
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-07-22 21:06:33 +0530

    Time: 5 ms (96.74%), Space: 100.7 MB (92.12%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -3,23 +3,28 @@ public:
     int maximumUniqueSubarray(vector<int>& nums) {
         int n = nums.size();
 
-        unordered_map<int, int> freq;
+        vector<int> cumSum(n, 0);
+        cumSum[0] = nums[0];
+
+        for(int i=1; i<n; i++){
+            cumSum[i] = cumSum[i-1] + nums[i];
+        }
+
+        vector<int> mp(10001, -1); // element -> index
 
         int l=0, r=0;
-        int currSum = 0;
         int ans = 0;
 
         while(r < n){
-            freq[nums[r]]++;
-            currSum = currSum + nums[r];
+            l = max(l, mp[nums[r]] + 1);
 
-            while(l < r && freq[nums[r]] > 1){
-                currSum = currSum - nums[l];
-                freq[nums[l]]--;
-                l++;
-            }
+            mp[nums[r]] = r;
+            
+            int sum_r = cumSum[r];
+            int sum_l = (l-1 < 0) ? 0 : cumSum[l-1];
 
-            ans = max(ans, currSum);
+            ans = max(ans, sum_r - sum_l);
+
             r++;
         }
 
