883d6d505d694a655a36d6f83b1df59c5db6988a
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-07-25 09:08:31 +0530

    Time: 4 ms (24.51%), Space: 13.3 MB (15.32%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,24 +3,32 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size();
         int n = nums2.size();
-        unordered_map<int,int> mp;
-        stack<int> st;
+
+        stack<int> st; // decreasing monotonic stack, iterating from right
+
+        vector<int> NGR(10001, -1);
 
         for(int i=n-1; i>=0; i--){
-            while(!st.empty() && st.top()<=nums2[i]){
+            while(!st.empty() && st.top() < nums2[i]){
                 st.pop();
             }
-            if(st.empty()) mp[nums2[i]] = -1;
-            else mp[nums2[i]] = st.top();
+
+            if(st.empty()){
+                NGR[nums2[i]] = -1;
+            }
+            else{
+                NGR[nums2[i]] = st.top();
+            }
 
             st.push(nums2[i]);
         }
 
-        vector<int> ans;
+        vector<int> ans(m, 0);
+
         for(int i=0; i<m; i++){
-            ans.push_back(mp[nums1[i]]);
+            ans[i] = NGR[nums1[i]];
         }
-        
+
         return ans;
     }
 };
