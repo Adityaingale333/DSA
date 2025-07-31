44f942f2f1f47eaded50524e51d7ef4a80508e46
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-07-31 07:13:57 +0530

    Time: 76 ms (14.99%), Space: 94.3 MB (12.68%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,53 +1,30 @@
 class Solution {
 public:
-    /*Let's see this with an example : 
-    nums = {2, 1}, k = 1
-    we have two subarrays having oddCount --> {2, 1}, {1}
-    COUNT_NICE_SUBARRAY = 2
-
-    Now, suppose we have one more integer 2 . our array looks like --> {2, 1, 2}, k = 1
-    If you see, addition of this extra 2 will not impact the oddCount because this 2 is even.
-    So, we again get two more subarrays by including this 2 with previous two nice subarrays {2, 1} and {1}
-    so we get {2, 1, 2}, {1, 2}
-    So, over all Nice subarrays becomes - {2, 1}, {1}, {2, 1, 2}, {1, 2}
-    Now, COUNT_NICE_SUBARRAY = 4
-
-
-    Now, let's take one more example : Let's say the one more integer after {2, 1} is 1
-    So, our array becomes -> {2, 1, 1}
-    Let's break it again -> from {2, 1} we had {2, 1}, {1} i.e. COUNT_NICE_SUBARRAY = 2
-
-    Now, when 1 comes, this is another Odd number so oddCount is affacted and hence we can't add previousCount = 2 to our result because {2, 1, 1} doesn't have k = 1 odd numbers. It has 2 odd numbers.
-    So, we need to start from fresh and hence prevCount is made 0.
-    Now, the new subarray having k = 1 oddCount is the {1} 
-    so we got one nice subarray now.
-    COUNT_NICE_SUBARRAY = 2 + 1 = 3 */
-
+    // we can also solve this using leetcode 560, subarray sum equals k
     int numberOfSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
 
-        int oddCount = 0;
-        int prevCount = 0;
-        int ans = 0;
+        unordered_map<int, int> mp; // freq of odd numbers -> how many times we have seen these odd numbers
+                                    // 0 -> 1,  1 -> 1,  2 -> 3,  3 -> 1
+        
+        mp[0] = 1; // before the start of the array we have seen 0 odd numbers, 1 time
 
         int l=0, r=0;
+        int oddCount = 0;
+        int ans = 0;
 
         while(r < n){
             if(nums[r] % 2 == 1){
                 oddCount++;
-                prevCount = 0;
             }
 
-            while(oddCount == k){
-                prevCount++;
+            int remOddCount = oddCount - k;
 
-                if(r < n && nums[l] % 2 == 1){
-                    oddCount--;
-                }
-                l++;
+            if(mp.count(remOddCount)){
+                ans = ans + mp[remOddCount];
             }
 
-            ans = ans + prevCount;
+            mp[oddCount]++;
             r++;
         }
 
