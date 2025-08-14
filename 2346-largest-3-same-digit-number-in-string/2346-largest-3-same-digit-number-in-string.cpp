aaa2fbb2c7ba51d7feec866f244cdf7988ec734d
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-08-14 09:12:49 +0530

    Time: 0 ms (100%), Space: 9 MB (77.37%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
@@ -4,9 +4,9 @@ public:
         int n = num.length();
 
         string ans = "";
-
+        
         for(int i=0; i<n-2; i++){
-            if(num[i] == num[i+1] && num[i+1] == num[i+2]){
+            if(num[i] == num[i+1] && num[i] == num[i+2]){
                 string curr = num.substr(i, 3);
                 if(curr > ans){
                     ans = curr;
