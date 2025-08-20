19573ded867eb6820165fbe444fc56564b6e6a5b
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-08-20 15:54:49 +0530

    Time: 29 ms (6.21%), Space: 31.6 MB (6.35%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
+    int solve(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& t){
+        if(i>=matrix.size() || j>=matrix[0].size() ||matrix[i][j] == 0){
+            return 0;
+        }
+
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
+        int right = solve(i, j+1, matrix, t);
+        int dia = solve(i+1, j+1, matrix, t);
+        int down = solve(i+1, j, matrix, t);
+
+        return t[i][j] = 1 + min({right, dia, down});
+    }
     int countSquares(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
 
-        vector<vector<int>> t(m, vector<int>(n, 0));
+        vector<vector<int>> t(m+1, vector<int>(n+1, -1));
 
         int ans = 0;
-        for(int i=0; i<m; i++){
-            t[i][0] = matrix[i][0];
-            ans = ans + t[i][0];
-        }
-        for(int j=1; j<n; j++){ // start from 1 to avoid double counting of t[0][0]
-            t[0][j] = matrix[0][j];
-            ans = ans + t[0][j];
-        }
 
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                if(matrix[i][j] == 0){
-                    t[i][j] = 0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 1){
+                    ans = ans + solve(i, j, matrix, t);
                 }
-                else{
-                    // +1 to khudke liye, aur upar niche or side me jo bhi minimun hoga wo
-                    // kyuki agar 3no me se koi ek bhi 1 nahi hua to square nahi ban skta 
-                    t[i][j] = min({t[i-1][j], t[i][j-1], t[i-1][j-1]}) + 1; 
-                } 
-                ans = ans + t[i][j];
             }
         }
 
