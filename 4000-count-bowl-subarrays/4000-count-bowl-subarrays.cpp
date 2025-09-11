f31bda18b89bfb4473bdb2b8ae44efcdcae6ed98
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-09-11 10:19:42 +0530

    Time: 8 ms (94.95%), Space: 134.2 MB (69.17%) - LeetHub

--- 4000-count-bowl-subarrays/4000-count-bowl-subarrays.cpp
+++ 4000-count-bowl-subarrays/4000-count-bowl-subarrays.cpp
@@ -1,69 +1,36 @@
 class Solution {
 public:
-    vector<int> get_NGEER(vector<int>& nums){
-        int n = nums.size();
-
-        stack<int> st;
-
-        vector<int> ngee(n, -1); // iterate from right, decreasing stack
-        for(int i=n-1; i>=0; i--){
-            while(!st.empty() && nums[st.top()] < nums[i]){
-                st.pop();
-            }
-
-            if(st.empty()){
-                ngee[i] = i;
-            }
-            else{
-                ngee[i] = st.top();
-            }
-
-            st.push(i);
-        }
-
-        return ngee;
-    }
-
-    vector<int> get_PGEEL(vector<int>& nums){
-        int n = nums.size();
-
-        stack<int> st;
-
-        vector<int> pgee(n, -1); // iterate from left, decreasing stack
-        for(int i=0; i<n; i++){
-            while(!st.empty() && nums[st.top()] < nums[i]){
-                st.pop();
-            }
-
-            if(st.empty()){
-                pgee[i] = i;
-            }
-            else{
-                pgee[i] = st.top();
-            }
-
-            st.push(i);
-        }
-
-        return pgee;
-    }
     long long bowlSubarrays(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> ngee = get_NGEER(nums);
-        vector<int> pgee = get_PGEEL(nums);
+        int maxVal = -1;
 
-        long long ans = 0;
+        vector<int> prefixGreater(n, 0);
         for(int i=0; i<n; i++){
-            if(ngee[i] != -1 && ngee[i] > i+1){
-                ans++;
+            if(maxVal > nums[i]){
+                prefixGreater[i] = 1;
             }
+            maxVal = max(nums[i], maxVal);
+        }
 
-            if(pgee[i] != -1 && nums[pgee[i]] != nums[i] && pgee[i] < i-1){
-                ans++;
+        maxVal = -1;
+
+        vector<int> suffixGreater(n, 0);
+        for(int i=n-1; i>=0; i--){
+            if(maxVal > nums[i]){
+                suffixGreater[i] = 1;
+            }
+            maxVal = max(nums[i], maxVal);
+        }
+        
+        long long count = 0;
+
+        for(int i=0; i<n; i++){
+            if(prefixGreater[i] && suffixGreater[i]){
+                count++;
             }
         }
 
-        return ans;
+        return count;
     }
 };
