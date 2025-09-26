878482c4297d677af95b88ec651a31bfd3c6abb8
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-09-26 23:00:17 +0530

    Time: 31 ms (69.53%), Space: 16.7 MB (42.88%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-    int bs(int low, int high, vector<int>& nums, int sum){
-        int idx = -1;
-
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] < sum){
-                idx = mid;
-                low = mid + 1;
-            }
-            else{
-                high = mid - 1;
-            }
-        }
-
-        return idx;
-    }
     int triangleNumber(vector<int>& nums) {
         int n = nums.size();
 
@@ -27,13 +11,18 @@ public:
 
         int ans = 0;
 
-        for(int i=0; i<n-2; i++){
-            for(int j=i+1; j<n-1; j++){
-                int sum = nums[i] + nums[j];
+        for(int k=n-1; k>=2; k--){
+            int i = 0;
+            int j = k-1;
 
-                int idx = bs(j+1, n-1, nums, sum);
-
-                if(idx != -1) ans = ans + (idx-j);
+            while(i < j){
+                if(nums[i] + nums[j] > nums[k]){
+                    ans = ans + (j-i);
+                    j--;
+                }
+                else{
+                    i++;
+                }
             }
         }
 
