660c3b27d70475d5bf9291a47b8f0cb36e7d66f8
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-09-30 14:02:30 +0530

    Time: 370 ms (17.71%), Space: 382.6 MB (15.02%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
@@ -8,7 +8,7 @@ public:
             for(int i=0; i<nums.size()-1; i++){
                 temp.push_back((nums[i] + nums[i+1]) % 10);
             }
-            nums = temp;
+            nums = move(temp);
         }
 
         return nums[0];
