797dd2142ff4299873ae4bd05405d5555edf4b32
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-11-14 19:38:00 +0530

    Time: 44 ms (45.77%), Space: 86.5 MB (60.5%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -5,15 +5,22 @@ public:
 
         vector<vector<int>> mat(n, vector<int>(n, 0));
 
-        for(int i=0; i<size; i++){
-            int row1 = queries[i][0];
-            int col1 = queries[i][1];
-            int row2 = queries[i][2];
-            int col2 = queries[i][3];
-            for(int j=row1; j<=row2; j++){
-                for(int k=col1; k<=col2; k++){
-                    mat[j][k]++;
+        for(auto& query : queries){
+            int row1 = query[0];
+            int col1 = query[1];
+            int row2 = query[2];
+            int col2 = query[3];
+            for(int i=row1; i<=row2; i++){
+                mat[i][col1] = mat[i][col1] + 1;
+                if(col2 + 1 < n){
+                    mat[i][col2 + 1] = mat[i][col2 + 1] - 1;
                 }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=1; j<n; j++){
+                mat[i][j] = mat[i][j] + mat[i][j-1];
             }
         }
 
