bd083b73a626d791453c946f149b79df2fa40759
Adityaingale333 <115146647+Adityaingale333@users.noreply.github.com>
2025-12-04 23:55:49 +0530

    Time: 8 ms (78.26%), Space: 19.1 MB (96.23%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     int countCollisions(string directions) {
-        int n = directions.size();
+        int n = directions.length();
 
-        int ans = 0;
-        for(int i=0; i<n-1; i++){
-            if(directions[i] == 'R' && directions[i+1] == 'L'){
-                ans += 2;
-                directions[i] = 'S';
-                directions[i+1] = 'S';
-            }
-            else if(directions[i] == 'S' && directions[i+1] == 'L'
-                    || directions[i] == 'R' && directions[i+1] == 'S'){
-                ans += 1;
-                directions[i] = 'S';
-                directions[i+1] = 'S';
-            }
+        int i = 0;
+        while(i<n && directions[i] == 'L'){
+            i++;
         }
 
-        for(int i=n-1; i>0; i--){
-            if(directions[i] == 'L' && directions[i-1] == 'R'){
-                ans += 2;
-                directions[i] = 'S';
-                directions[i-1] = 'S';
+        int j = n-1;
+        while(j>=0 && directions[j] == 'R'){
+            j--;
+        }
+
+        int ans = 0;
+        while(i <= j){
+            if(directions[i] != 'S'){
+                ans++;
             }
-            else if(directions[i] == 'S' && directions[i-1] == 'R'
-                    || directions[i] == 'L' && directions[i-1] == 'S'){
-                ans += 1;
-                directions[i] = 'S';
-                directions[i-1] = 'S';
-            }
-        } 
+            i++;
+        }
+
         return ans;
     }
 };
